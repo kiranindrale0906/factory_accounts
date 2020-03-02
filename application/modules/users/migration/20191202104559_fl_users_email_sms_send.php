37202decc77028cdd0d20364985767be6248f746
pradeep kotiyan <pradeep.kotiyan@ascratech.in>
2020-03-02 19:29:36 +0530

    changes

--- application/modules/users/migration/20191202104559_fl_users_email_sms_send.php
+++ application/modules/users/migration/20191202104559_fl_users_email_sms_send.php
@@ -7,7 +7,7 @@ class Migration_fl_users_email_sms_send extends CI_Model {
   public function up()
   {
   	$sql="alter table users add  email_sent_at DATETIME,add sms_sent_at DATETIME";
-    $this->db->query($sql);
+    //$this->db->query($sql);
   }
 
 
