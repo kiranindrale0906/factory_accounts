e7758db668e70fe23b25be3129e1d4e07562c01d
pradeep kotiyan <pradeep.kotiyan@ascratech.in>
2020-03-02 19:02:59 +0530

    migration files added

---
+++ application/modules/masters/migrations/20200302182607_ac_customer_category.php
@@ -0,0 +1,31 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_ac_customer_category extends CI_Model {
+
+  public function up()
+  {
+  	$sql="CREATE TABLE `ac_customer_category` (
+				  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+				  `company_id` int(11) DEFAULT NULL ,
+				  `category_name` varchar(255) DEFAULT NULL,
+				  `category_name_id` int(11) DEFAULT NULL,
+				  `department_name` varchar(255) DEFAULT NULL,
+				  `department_name_id` int(11) DEFAULT NULL,
+				  `account_name` varchar(255) DEFAULT NULL,
+				  `account_name_id` int(11) DEFAULT NULL,
+				  `wastage` int(11) DEFAULT NULL,
+				  `created_at` datetime NOT NULL,
+				  `updated_at` datetime NOT NULL,
+				  `is_delete` tinyint(1) DEFAULT '0',
+				  `created_by` int(11) DEFAULT '0',
+				  `updated_by` int(11) DEFAULT '0'
+				) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+    $this->db->query($sql);
+  }
+
+
+}
+
+?>
---
+++ application/modules/masters/migrations/20200302183609_ac_purity.php
@@ -0,0 +1,25 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_ac_purity extends CI_Model {
+
+  public function up()
+  {
+  	$sql = "CREATE TABLE `ac_purity` (
+					  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+					  `purity` float(10,2) NOT NULL,
+					  `company_id` int(11) NOT NULL,
+					  `created_at` datetime DEFAULT NULL,
+					  `updated_at` datetime DEFAULT NULL,
+					  `is_delete` tinyint(1) DEFAULT '0',
+					  `created_by` int(11) DEFAULT '0',
+					  `updated_by` int(11) DEFAULT '0'
+					) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+    $this->db->query($sql);
+  }
+
+
+}
+
+?>
