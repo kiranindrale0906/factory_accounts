82ac0564e45e40da14276b71c3df30aa4758d694
pradeep kotiyan <pradeep.kotiyan@ascratech.in>
2020-03-02 19:26:05 +0530

    migration added of ac user

--- application/modules/masters/migrations/20200215140700_add_field_sign_in_sign_ip.php
+++ application/modules/masters/migrations/20200215140700_add_field_sign_in_sign_ip.php
@@ -7,7 +7,7 @@ class Migration_add_field_sign_in_sign_ip extends CI_Model {
   public function up()
   {
   	$sql ="alter table ac_users add `last_sign_in_at` datetime,add `last_sign_in_ip` varchar(255)";
-    $this->db->query($sql);
+    //$this->db->query($sql);
   }
 }
 
