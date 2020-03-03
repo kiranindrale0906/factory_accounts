56989978ac9b8b8cb76338eab8b23d03784de530
pradeep kotiyan <pradeep.kotiyan@ascratech.in>
2020-03-03 10:30:45 +0530

    sidebar menu modified

--- application/modules/ac_vouchers/views/ac_vouchers/tabs.php
+++ application/modules/ac_vouchers/views/ac_vouchers/tabs.php
@@ -1,8 +1,57 @@
+<?php if($this->router->class =="cash_receipt_vouchers" || $this->router->class=="cash_issue_vouchers" || $this->router->class =="metal_receipt_vouchers" || $this->router->class=="metal_issue_vouchers") : 
+	$class_issue= "";
+	$class_receipt= "";
+	//if(!empty($this->router->class == "cash_receipt_vouchers")) {
+	if(!empty(strrpos($this->router->class, "receipt") == false)) {
+		$class_receipt="btn-success";
+	}
+	else
+	{
+		$class_issue="btn-danger";
+	}
+
+	$label_issue=str_replace("receipt","issue", $this->router->class);
+	$label_receipt=str_replace("issue","receipt", $this->router->class);
+?> 
+
 <div class="col-lg-6 col-md-12">
   <div class="form-group row">
     <div class="btn-group" role="group" aria-label="Basic example">
-      <a href="<?= base_url('transactions/cash_receipt_vouchers') ?>" class="btn btn-sm text-uppercase btn-secondary skewright"><span class="skewleft">Cash Receipt</span></a>
-      <a href="<?= base_url('transactions/cash_issue_vouchers'); ?>" class="btn btn-sm text-uppercase btn-danger skewright"><span class="skewleft">Cash Issue</span></a>
+      <a href="<?= base_url('transactions/'.str_replace('receipt','issue', $this->router->class)) ?>" class="btn btn-sm btn-secondary text-uppercase <?= $class_receipt; ?> skewright">
+      	<span class="skewleft">
+      		<?= str_replace("_", " ", $label_issue); ?> <!-- Cash Receipt -->
+      	</span>
+      </a>
+      <a href="<?= base_url('transactions/'.str_replace('issue','receipt', $this->router->class)); ?>" class="btn btn-sm btn-secondary text-uppercase <?= $class_issue; ?> skewright">
+      	<span class="skewleft"><?= str_replace("_", " ", $label_receipt); ?>
+      	</span>
+      </a>
     </div>
   </div>
 </div>
+
+<?php endif; ?>
+
+<?php if($this->router->class =="metal_receipt_vouchers" || $this->router->class=="metal_issue_vouchers") : 
+
+	// if(!empty($this->router->class == "metal_receipt_vouchers")) {
+	// 	$class_issue="btn-secondary";
+	// 	$class_receipt="btn-success";
+	// }
+	// else
+	// {
+	// 	$class_issue="btn-danger";
+	// 	$class_receipt="btn-secondary";	
+	// }
+?> 
+<!-- 
+<div class="col-lg-6 col-md-12">
+  <div class="form-group row">
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <a href="<?= base_url('transactions/metal_receipt_vouchers') ?>" class="btn btn-sm text-uppercase <?= $class_receipt; ?> skewright"><span class="skewleft">Metal Receipt</span></a>
+      <a href="<?= base_url('transactions/metal_issue_vouchers'); ?>" class="btn btn-sm text-uppercase <?= $class_issue; ?> skewright"><span class="skewleft">Metal Issue</span></a>
+    </div>
+  </div>
+</div> -->
+
+<?php endif; ?>
