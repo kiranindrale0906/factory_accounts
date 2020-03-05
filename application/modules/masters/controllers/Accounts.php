ec35addbd68dd20e6bf2d94ebd948a66c0602e4b
pradeep kotiyan <pradeep.kotiyan@ascratech.in>
2020-03-05 19:49:30 +0530

    changes

--- application/modules/masters/controllers/Accounts.php
+++ application/modules/masters/controllers/Accounts.php
@@ -12,7 +12,7 @@ class Accounts extends BaseController {
 
   public function index() {
     if(!empty($_POST['account_name'])) {
-      $data=$this->model->get('name');
+      $data=$this->model->get('name',array('name like "%'.$_POST['account_name'].'%"'=>NULL));
       echo json_encode(array('data'=>$data,'status'=>'success')); die;
     }
 
