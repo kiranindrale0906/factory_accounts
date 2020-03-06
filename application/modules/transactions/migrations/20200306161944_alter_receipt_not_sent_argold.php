8341f85b270173f98861c68f317c683ac353519d
pradeep kotiyan <pradeep.kotiyan@ascratech.in>
2020-03-06 16:21:02 +0530

    migration modified

---
+++ application/modules/transactions/migrations/20200306161944_alter_receipt_not_sent_argold.php
@@ -0,0 +1,16 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_alter_receipt_not_sent_argold extends CI_Model {
+
+  public function up()
+  {
+  	$sql= "alter table receipt_not_sent_argold add api_url varchar(255)";
+    $this->db->query($sql);
+  }
+
+
+}
+
+?>
