5b89f85ad4eff425db4d3413f0e3cfe69c0d16af
pradeep kotiyan <pradeep.kotiyan@ascratech.in>
2020-03-06 18:49:18 +0530

    changes

---
+++ application/modules/reports/views/account_ledger_reports/thead.php
@@ -0,0 +1,13 @@
+<thead>
+  <tr>
+    <th class="text-center">Name</th>
+    <th class="text-center">Date</th>
+    <th class="text-center">Desc</th>
+    <th class="text-center">Gross Wt</th>  
+    <th class="text-center">Melting</th>  
+    <th class="text-center">Purity</th>  
+    <th class="text-center">Factory Melting</th>  
+    <th class="text-center">Factory Purity</th>  
+    <th class="text-center">Difference</th>  
+  </tr>
+</thead>
