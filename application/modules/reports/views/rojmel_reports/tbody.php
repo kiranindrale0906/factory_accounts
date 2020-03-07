987e20557d90de45b7c7edfb2ff414e38cadb3ec
pradeep kotiyan <pradeep.kotiyan@ascratech.in>
2020-03-07 16:39:00 +0530

    changes

--- application/modules/reports/views/rojmel_reports/tbody.php
+++ application/modules/reports/views/rojmel_reports/tbody.php
@@ -1,34 +1,34 @@
 <tbody>
   <?php 
-    $this->load->view('reports/account_ledger_reports/balance', 
+    $this->load->view('reports/rejmel_reports/balance', 
                       array('label' => 'Opening',
                            'voucher_date' => $previous_date,
                            'type' => $type));
 
     foreach ($voucher_date_records as $index => $record){
       if(!strpos($record['voucher_type'],'issue'))
-        $this->load->view('reports/account_ledger_reports/tr_receipt', array('record' => $record));   
+        $this->load->view('reports/rejmel_reports/tr_receipt', array('record' => $record));   
       else 
-        $this->load->view('reports/account_ledger_reports/tr_issue', array('record' => $record));  
+        $this->load->view('reports/rejmel_reports/tr_issue', array('record' => $record));  
     } 
     
     if(!strpos($record['voucher_type'],'issue')) {
-      $this->load->view('reports/account_ledger_reports/total_receipts', 
+      $this->load->view('reports/rejmel_reports/total_receipts', 
                         array('label' => 'Total',
                              'debit_weight' => $total[$voucher_date][$type]['debit_weight'], 
                              'weight_difference' => $total[$voucher_date][$type]['weight_difference']));
 
-      $this->load->view('reports/account_ledger_reports/balance', 
+      $this->load->view('reports/rejmel_reports/balance', 
                         array('label' => 'Balance',
                              'voucher_date' => $voucher_date,
                              'type' => $type));
     }
     else {
-      $this->load->view('reports/account_ledger_reports/total_issues', 
+      $this->load->view('reports/rejmel_reports/total_issues', 
                             array('label' => 'Total',
                                  'credit_weight' => $total[$voucher_date][$type]['credit_weight'],
                                  'weight_difference' => $total[$voucher_date][$type]['weight_difference']));
-      $this->load->view('reports/account_ledger_reports/balance', 
+      $this->load->view('reports/rejmel_reports/balance', 
                         array('label' => 'Balance',
                              'voucher_date' => $voucher_date,
                              'type' => $type));
