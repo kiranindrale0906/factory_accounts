d3b92356194c1ced26b2e6660d1c0f6d40b54013
pradeep kotiyan <pradeep.kotiyan@ascratech.in>
2020-03-07 18:09:41 +0530

    changes

--- application/modules/reports/views/account_ledger_reports/tr_issue.php
+++ application/modules/reports/views/account_ledger_reports/tr_issue.php
@@ -3,9 +3,9 @@
   <td><?= $record['voucher_date'] ?></td>
   <td><?= $record['narration'];?></td>
   <td class="text-right"><?= $record['credit_weight'] ?></td>
-  <td class="text-right"><?=($record['credit_weight']*$record['purity'])/100 ?></td>
-  <td class="text-right"><?= $record['credit_weight']*$record['purity']; ?></td>
-  <td class="text-right"><?=($record['credit_weight']*$record['factory_purity'])/100?></td>
-  <td class="text-right"><?= $record['credit_weight']*$record['factory_purity']; ?></td>
+  <td class="text-right"><?=$record['purity'] ?></td>
+  <td class="text-right"><?= $record['credit_weight']*$record['purity']/100; ?></td>
+  <td class="text-right"><?=$record['factory_purity'] ?></td>
+  <td class="text-right"><?= $record['credit_weight']*$record['factory_purity']/100; ?></td>
   <td class="text-right"><?=($record['purity']-$record['factory_purity'])*$record['credit_weight']/100 ?></td>
 </tr>
