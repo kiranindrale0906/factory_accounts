507a188c0f6c913ef9ebc8b5128e3387ec9a0d29
pradeep.kotiyan <pradeep.kotiyan@ascratech.in>
2020-03-12 19:43:39 +0530

    client account ledger report modified

--- application/modules/reports/views/client_account_ledger_reports/thead.php
+++ application/modules/reports/views/client_account_ledger_reports/thead.php
@@ -1,13 +1,7 @@
 <thead>
   <tr>
     <th>Account Name</th>
-    <th>Date</th>
-    <th>Desc</th>
     <th>Gross Wt</th>
-    <th>Melting</th>
     <th>Fine</th>
-    <th>Factory Melting</th>
-    <th>Factory Fine</th>
-    <th>Difference</th>
   </tr>
 </thead>
