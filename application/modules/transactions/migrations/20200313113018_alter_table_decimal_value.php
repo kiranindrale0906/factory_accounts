25097708f62575bd5adf199afe58d52e1a9dc1f5
pradeep.kotiyan <pradeep.kotiyan@ascratech.in>
2020-03-13 11:38:12 +0530

    changes

--- application/modules/transactions/migrations/20200313113018_alter_table_decimal_value.php
+++ application/modules/transactions/migrations/20200313113018_alter_table_decimal_value.php
@@ -6,7 +6,7 @@ class Migration_alter_table_decimal_value extends CI_Model {
 
   public function up()
   {
-  	$sql = "alter table ac_vouchers modify credit_amount float(14,4),modify debit_amount float(14,4),modify amount float(14,4),modify purity float(14,4),modify pure_gold_credit float(14,4),modify pure_gold_debit float(14,4),modify factory_purity float(14,4),modify credit_weight float(14,4),modify debit_weight float(14,4),modify factory_purity float(14,4)";
+  	$sql = "alter table ac_vouchers modify credit_amount float(14,4),modify debit_amount float(14,4),modify amount float(14,4),modify purity float(14,4),modify pure_gold_credit float(14,4),modify pure_gold_debit float(14,4),modify factory_purity float(14,4),modify credit_weight float(14,4),modify debit_weight float(14,4),modify purity_margin float(14,4),modify fine float(14,4),modify factory_fine float(14,4)";
     $this->db->query($sql);
   }
 
