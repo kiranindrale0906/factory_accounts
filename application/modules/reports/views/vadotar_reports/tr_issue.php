d4b6616d7254f333488a39a58c7b2ef085545be0
www-data <www-data@ip-172-26-3-146.ap-south-1.compute.internal>
2020-03-13 14:19:50 +0530

    vadotar opening added

--- application/modules/reports/views/vadotar_reports/tr_issue.php
+++ application/modules/reports/views/vadotar_reports/tr_issue.php
@@ -7,5 +7,9 @@
   <td class="text-right"><?= four_decimal($record['credit_weight']*$record['purity']/100); ?></td>
   <td class="text-right"><?= four_decimal($record['factory_purity']) ?></td>
   <td class="text-right"><?= four_decimal($record['credit_weight']*$record['factory_purity']/100); ?></td>
-  <td class="text-right"><?= four_decimal(($record['purity']-$record['factory_purity'])*$record['credit_weight']/100); ?></td>
+  <?php if ($record['account_name'] == 'Opening') { ?>
+    <td class="text-right">74180.7900</td>
+  <?php } else { ?>
+    <td class="text-right"><?= four_decimal(($record['purity']-$record['factory_purity'])*$record['credit_weight']/100); ?></td>
+  <?php } ?>
 </tr>
