d8972987d78c678dc2c8dd06e5bffb24971983cf
pradeep kotiyan <pradeep.kotiyan@ascratech.in>
2020-04-09 23:00:21 +0530

    client wise provision for metal issue and metal receipt voucher

--- application/modules/transactions/views/metal_issue_vouchers/view.php
+++ application/modules/transactions/views/metal_issue_vouchers/view.php
@@ -1,1 +1,8 @@
-<?php $this->load->view('ac_vouchers/ac_vouchers/view'); ?>
+<?php //$this->load->view('ac_vouchers/ac_vouchers/view'); ?>
+<?php
+  $file_exist_main_path=@file_exists(APPPATH."modules/".CLIENT_NAME."/views/metal_issue_voucher_clients/view.php");
+  if($file_exist_main_path) 
+    $this->load->view(CLIENT_NAME."/metal_issue_voucher_clients/view.php");
+  else
+    $this->load->view("ac_vouchers/ac_vouchers/view.php");
+?>
