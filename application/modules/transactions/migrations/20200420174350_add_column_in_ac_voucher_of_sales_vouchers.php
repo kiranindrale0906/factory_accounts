c889e4113dbbf79f9a5601b8baca9552b4e9629d
pradeep kotiyan <pradeep.kotiyan@ascratech.in>
2020-04-20 18:59:37 +0530

    migration file added

--- application/modules/transactions/migrations/20200420174350_add_column_in_ac_voucher_of_sales_vouchers.php
+++ application/modules/transactions/migrations/20200420174350_add_column_in_ac_voucher_of_sales_vouchers.php
@@ -16,15 +16,16 @@ class Migration_add_column_in_ac_voucher_of_sales_vouchers extends CI_Model {
     $this->db->query("ALTER TABLE `ac_vouchers` ADD `to_group_id` int(11) NOT NULL;");
     $this->db->query("ALTER TABLE `ac_vouchers` ADD `has_hallmark` int(11) NOT NULL;");
     $this->db->query("ALTER TABLE `ac_vouchers` ADD `hallmark_number` decimal(10,4) NOT NULL;");
-    $this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `category` VARCHAR(225) NOT NULL;")
-	$this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `moti_wt` DECIMAL(10,4) NOT NULL ;");
-	$this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `melting` DECIMAL(10,4) NOT NULL ;");
-	$this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `wastage` DECIMAL(10,4) NOT NULL ;");
-	$this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `rate` DECIMAL(10,4) NOT NULL ;");
-	$this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `gold_amount` DECIMAL(10,4) NOT NULL ;");
-	$this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `labour_rate` DECIMAL(10,4) NOT NULL ;");
-	$this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `other_charges` DECIMAL(10,4) NOT NULL ;");
-	$this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `description` DECIMAL(10,4) NOT NULL ;");
+
+    $this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `category` VARCHAR(225) NOT NULL;");
+	 $this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `moti_wt` DECIMAL(10,4) NOT NULL ;");
+	 $this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `melting` DECIMAL(10,4) NOT NULL ;");
+	 $this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `wastage` DECIMAL(10,4) NOT NULL ;");
+	 $this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `rate` DECIMAL(10,4) NOT NULL ;");
+	 $this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `gold_amount` DECIMAL(10,4) NOT NULL ;");
+	 $this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `labour_rate` DECIMAL(10,4) NOT NULL ;");
+	 $this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `other_charges` DECIMAL(10,4) NOT NULL ;");
+	 $this->db->query("ALTER TABLE `ac_sales_purchase_repair_voucher` ADD `description` DECIMAL(10,4) NOT NULL ;");
   }
 
 
