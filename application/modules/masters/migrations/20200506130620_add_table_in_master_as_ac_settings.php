ce7786d7c09267e7acdad9cae1d7cd6ec27b14f9
kiran <kiran.indrale@ascratech.com>
2020-05-06 13:35:38 +0530

    change in settings

--- application/modules/masters/migrations/20200506130620_add_table_in_master_as_ac_settings.php
+++ application/modules/masters/migrations/20200506130620_add_table_in_master_as_ac_settings.php
@@ -6,16 +6,16 @@ class Migration_add_table_in_master_as_ac_settings extends CI_Model {
 
   public function up()
   {
-    $this->db->query("CREATE TABLE `ac_settings` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `value` varchar(255) NOT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  `is_delete` tinyint(1) DEFAULT '0',
-  `created_by` int(11) DEFAULT '0',
-  `updated_by` int(11) DEFAULT '0'
-);");
+//     $this->db->query("CREATE TABLE `ac_settings` (
+//   `id` int(11) NOT NULL,
+//   `name` varchar(255) NOT NULL,
+//   `value` varchar(255) NOT NULL,
+//   `created_at` datetime DEFAULT NULL,
+//   `updated_at` datetime DEFAULT NULL,
+//   `is_delete` tinyint(1) DEFAULT '0',
+//   `created_by` int(11) DEFAULT '0',
+//   `updated_by` int(11) DEFAULT '0'
+// );");
     
   }
 
