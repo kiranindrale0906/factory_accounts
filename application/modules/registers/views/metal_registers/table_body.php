24392194a7fe560ca01e11404332d7f21db01bae
pradeep kotiyan <pradeep.kotiyan@ascratech.in>
2020-05-08 18:17:29 +0530

    metal register

--- application/modules/registers/views/metal_registers/table_body.php
+++ application/modules/registers/views/metal_registers/table_body.php
@@ -1,8 +1,27 @@
-<tr>
-    <td><?= date('d-m-y', strtotime($voucher_date)) ?></td>
-    <td><?= $voucher_number ?></td>
-    <td><?= $account_name ?></td>
-    <td class="text-right"><?= (!empty($debit_weight)) ? decimal_number_format($credit_weight):"0.0000"; ?></td>
-    <td class="text-right"><?= (!empty($debit_weight)) ? decimal_number_format($debit_weight) : "0.0000"; ?></td>
-    <td><?= (!empty($narration)) ? $narration : '' ?></td>
+<?php
+$grand_credit = 0;
+$grand_debit = 0;
+$grand_balance = 0; 
+	
+foreach ($metal_register as $key => $metal_data) {
+ $grand_credit += !empty($metal_data['credit_weight'])?$metal_data['credit_weight']:0;
+ $grand_debit += !empty($metal_data['debit_weight'])?$metal_data['debit_weight']:0; ?>
+
+	<tr>
+	    <td><?= date('d-m-y', strtotime($metal_data['voucher_date'])) ?></td>
+	    <td><?= $metal_data['voucher_number'] ?></td>
+	    <td><?= $metal_data['account_name'] ?></td>
+	    <td class="text-right"><?= (!empty($metal_data['credit_weight'])) ? decimal_number_format($metal_data['credit_weight']):"0.0000"; ?></td>
+	    <td class="text-right"><?= (!empty($metal_data['debit_weight'])) ? decimal_number_format($metal_data['debit_weight']) : "0.0000"; ?></td>
+	    <td><?= (!empty($metal_data['narration'])) ? $metal_data['narration'] : '' ?></td>
+	</tr>
+
+<?php
+	}
+?>
+ <tr>
+  <td class="text-right" colspan="3"><b>Total</b></td>
+  <td class="text-right"><b><?= decimal_number_format($grand_credit) ?></b></td>
+  <td class="text-right"><b><?= decimal_number_format($grand_debit) ?></b></td>
+  <td></td>
 </tr>
