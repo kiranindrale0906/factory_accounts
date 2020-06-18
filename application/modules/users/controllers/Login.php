9ffea432aaff6b37d88a7d1bdcbbee3ac6593c90
kiran <kiran.indrale@ascratech.com>
2020-06-18 11:10:31 +0530

    chnag

--- application/modules/users/controllers/Login.php
+++ application/modules/users/controllers/Login.php
@@ -17,6 +17,17 @@ class Login extends Core_login {
   } 
 
   public function _after_save($formdata, $action) {
+     $this->db->query("CREATE TABLE `ac_sub_groups` (
+        `id` int(11) NOT NULL,
+        `name` varchar(225) NOT NULL,
+        `group_id` int(11) NOT NULL,
+        `group_name` varchar(225) NOT NULL,
+        `created_by` int(11) NOT NULL,
+        `created_at` datetime NOT NULL,
+        `updated_by` int(11) NOT NULL,
+        `updated_at` datetime NOT NULL,
+        `is_delete` int(11) NOT NULL DEFAULT '0'
+      )");
     $user_data = $this->User_model->set_user_data_in_session(
     							array("email_id" => $formdata['login']['email_id']));
     if(!is_api_request()) {
