8062a0dbee0e699c6c01e9b59a5d23a198a5b064
kiran <kiran.indrale@ascratech.com>
2020-06-22 15:57:51 +0530

    change

--- application/modules/transactions/controllers/Metal_issue_vouchers.php
+++ application/modules/transactions/controllers/Metal_issue_vouchers.php
@@ -13,7 +13,6 @@ if (file_exists(APPPATH . "modules/".CLIENT_NAME."/controllers/Client_metal_issu
 } else {
   
   require_once APPPATH . "modules/ac_vouchers/controllers/Core_metal_issue_vouchers.php";
-  require_once APPPATH . "modules/ac_vouchers/models/Core_metal_issue_vouchers.php";  
   class Metal_issue_vouchers extends Core_metal_issue_vouchers {
     function __construct($data=array()) {
       parent::__construct($data);
--- application/modules/transactions/controllers/Metal_receipt_vouchers.php
+++ application/modules/transactions/controllers/Metal_receipt_vouchers.php
@@ -13,7 +13,6 @@ if (file_exists(APPPATH . "modules/".CLIENT_NAME."/controllers/Client_metal_rece
 } else {
   
   require_once APPPATH . "modules/ac_vouchers/controllers/Core_metal_receipt_vouchers.php";
-  require_once APPPATH . "modules/ac_vouchers/models/Core_metal_receipt_vouchers.php";  
   class Metal_receipt_vouchers extends Core_metal_receipt_vouchers {
     function __construct($data=array()) {
       parent::__construct($data);
