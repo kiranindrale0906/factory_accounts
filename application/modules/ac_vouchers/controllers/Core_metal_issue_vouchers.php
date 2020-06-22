f01ecd37e07280d5d4deba012ebcdcdf8c501523
kiran <kiran.indrale@ascratech.com>
2020-06-22 15:23:03 +0530

    chnage

--- application/modules/ac_vouchers/controllers/Core_metal_issue_vouchers.php
+++ application/modules/ac_vouchers/controllers/Core_metal_issue_vouchers.php
@@ -2,6 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 require_once APPPATH . "modules/ac_vouchers/controllers/Vouchers.php";
 class Core_metal_issue_vouchers extends Vouchers {
+  
 
   public function __construct() {
     parent::__construct();
