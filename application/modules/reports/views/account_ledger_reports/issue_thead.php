cbefdc1772eec7a8ca6f4ab06402018495e0c1ff
abdul <abdul@ascratech.com>
2020-06-23 15:25:34 +0530

    changes to labels

--- application/modules/reports/views/account_ledger_reports/issue_thead.php
+++ application/modules/reports/views/account_ledger_reports/issue_thead.php
@@ -3,11 +3,11 @@
     <th>Account Name</th>
     <th>Date</th>
     <th>Desc</th>
-    <th>Gross Wt</th>
-    <th>Factory Melting</th>
-    <th>Factory Fine</th>
-    <th>Issue Melting</th>
-    <th>Issue Fine</th>
-    <th>Vadotar</th>
+    <th class='text-right'>Gross Wt</th>
+    <th class='text-right'>Factory Melting</th>
+    <th class='text-right'>Factory Fine</th>
+    <th class='text-right'>Issue Melting</th>
+    <th class='text-right'>Issue Fine</th>
+    <th class='text-right'>Vadotar</th>
   </tr>
 </thead>
--- application/modules/reports/views/rojmel_reports/receipt_thead.php
+++ application/modules/reports/views/rojmel_reports/receipt_thead.php
@@ -3,11 +3,11 @@
     <th>Account Name</th>
     <th>Date</th>
     <th>Desc</th>
-    <th>Gross Wt</th>
-    <th>Melting</th>
-    <th>Fine</th>
-    <th>Factory Melting</th>
-    <th>Factory Fine</th>
-    <th>Vadotar</th>
+    <th class='text-right'>Gross Wt</th>
+    <th class='text-right'>Melting</th>
+    <th class='text-right'>Fine</th>
+    <th class='text-right'>Factory Melting</th>
+    <th class='text-right'>Factory Fine</th>
+    <th class='text-right'>Vadotar</th>
   </tr>
 </thead>
