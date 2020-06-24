e1d4810a1d846ce58840daf416113612c6bf7b7b
Atul Agrawal <atul@ascratech.com>
2020-06-24 12:37:05 +0530

    updates to column sequence in account ledger

--- application/modules/reports/views/account_ledger_reports/issue_thead.php
+++ application/modules/reports/views/account_ledger_reports/issue_thead.php
@@ -4,10 +4,10 @@
     <th>Date</th>
     <th>Desc</th>
     <th class='text-right'>Gross Wt</th>
-    <th class='text-right'>Factory Melting</th>
-    <th class='text-right'>Factory Fine</th>
     <th class='text-right'>Issue Melting</th>
     <th class='text-right'>Issue Fine</th>
-    <th class='text-right'>Vadotar</th>
+    <th class='text-right'>Factory Melting</th>
+    <th class='text-right'>Factory Fine</th>
+    <!-- <th class='text-right'>Vadotar</th> -->
   </tr>
 </thead>
--- application/modules/reports/views/account_ledger_reports/receipt_thead.php
+++ application/modules/reports/views/account_ledger_reports/receipt_thead.php
@@ -4,10 +4,10 @@
     <th>Date</th>
     <th>Desc</th>
     <th class='text-right'>Gross Wt</th>
-    <th class='text-right'>Melting</th>
-    <th class='text-right'>Fine</th>
     <th class='text-right'>Factory Melting</th>
     <th class='text-right'>Factory Fine</th>
-    <th class='text-right'>Vadotar</th>
+    <th class='text-right'>Melting</th>
+    <th class='text-right'>Fine</th>
+    <!-- <th class='text-right'>Vadotar</th> -->
   </tr>
 </thead>
