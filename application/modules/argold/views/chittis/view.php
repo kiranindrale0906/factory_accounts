63d95a246c0c907dacaee42bb29523e0ed0da6c2
kiran <kiran.indrale@ascratech.com>
2020-06-26 17:34:29 +0530

    chnage

--- application/modules/argold/views/chittis/view.php
+++ application/modules/argold/views/chittis/view.php
@@ -1,12 +1,12 @@
-<h5 class="heading">Chitti View</h5>
+<h5 class="heading noprint">Chitti View</h5>
 <div class="row">
-<div class="col-md-6 border-right">
+<div class="col-md-6 ">
     <div class="form-group container">
       <p><h6>AC Code :<?=$account_id?> </h6></p>
       <p><h6>Voucher No :<?=$record['id']?> </h6></p>
     </div>
   </div>
-  <div class="col-md-6 border-right">
+  <div class="col-md-6">
     
     <div class="form-group container">
       <p><h6>Date : <?=date('d-m-Y',strtotime($record['created_at']))?></h6></p>
@@ -15,14 +15,15 @@
   </div>
   
 </div>
-<hr>
+<hr class="noprint">
 <?php
   $this->load->view('chitti_details/viewlist');
 ?>
 <style type="text/css">
+
   @page { 
     margin: 0mm; 
-    size: 4in 6in ;
+    size: 4cm 6cm ;
     size: landscape;
   }
 </style>
