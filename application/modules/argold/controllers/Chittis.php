b0555ed462fea75fc30b1ea972c946dcf4717234
Atul Agrawal <atul@ascratech.com>
2020-06-26 15:18:20 +0530

    changes

--- application/modules/argold/controllers/Chittis.php
+++ application/modules/argold/controllers/Chittis.php
@@ -24,4 +24,20 @@ class Chittis extends BaseController {
     if(!empty($this->data['metal_voucher_details']['account_name']))
       $this->data['account_id'] = $this->account_model->find('id',array('name'=>$this->data['metal_voucher_details']['account_name']))['id'];
   }
+
+  public function _get_form_data() {
+    $this->data['record']['account_name']=!empty($_GET['account_name'])?$_GET['account_name']:"";
+    $where=array('voucher_type'=>'metal issue voucher','chitti_id'=>'');
+    if(!empty($this->data['record']['account_name'])){
+      $where['account_name']=$this->data['record']['account_name'];
+      $this->data['metal_vouchers'] = $this->voucher_model->get('',$where);
+    } else {
+      $this->data['metal_vouchers'] = array();
+    }
+    
+    if ($this->router->method == 'store' || $this->router->method == 'update') {
+      $this->data['record']['chittis'] = $_POST['chittis'];
+      $this->data['chittis_details'] = @$_POST['chittis_details'];
+    }
+  }
 }
