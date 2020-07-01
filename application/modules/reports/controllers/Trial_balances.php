ee816d361860336b3951092fe555747a61c16140
Atul Agrawal <atul@ascratech.com>
2020-07-01 11:14:18 +0530

    changes closing stock in trial balance

--- application/modules/reports/controllers/Trial_balances.php
+++ application/modules/reports/controllers/Trial_balances.php
@@ -11,12 +11,7 @@ class Trial_balances extends Ledgers {
 
   public function index() {
     $this->data['layout']='application';
-    $url=API_ARG_BASE_PATH."issue_and_receipts/ledger_balance/index";
-    $arg_records=json_decode(curl_post_request($url));
-    $url=API_LIVE_BASE_PATH."issue_and_receipts/ledger_balance/index";
-    $records=json_decode(curl_post_request($url));
-    $this->data['argold_balance']=$arg_records->data->record;
-    $this->data['live_balance']=$records->data->record;
+    
     $this->get_form_data();
     $this->get_account_ledger_records();
     $this->load->render($this->router->class."/index",$this->data);
@@ -24,6 +19,15 @@ class Trial_balances extends Ledgers {
 
   public function get_form_data() {
     $this->data['account_names'] = $this->model->get('distinct(account_name) as name', array(), array(), array('order_by'=>'account_name asc'));
+
+    $url=API_ARG_BASE_PATH."issue_and_receipts/ledger_balance/index";
+    $arg_records=json_decode(curl_post_request($url));
+    
+    $url=API_LIVE_BASE_PATH."issue_and_receipts/ledger_balance/index";
+    $records=json_decode(curl_post_request($url));
+    
+    $this->data['argold_balance']=$arg_records->data->record;
+    $this->data['live_balance']=$records->data->record;
   }
 
   private function get_account_ledger_records() {
