c899cdb0d3f1049d44848568410104679f526042
Atul Agrawal <atul@ascratech.com>
2020-07-03 17:23:02 +0530

    changes

--- application/modules/reports/controllers/Vadotar_reports.php
+++ application/modules/reports/controllers/Vadotar_reports.php
@@ -24,10 +24,10 @@ class Vadotar_reports extends Ledgers {
   }
 
   private function get_companywise_vadotar() {
-    $company_vadotars = $this->model->get('receipt_type, sum(((credit_weight + debit_weight) * (factory_purity - purity)) / 100 ) as vadotar', 
+    $company_vadotars = $this->model->get('receipt_type, sum(factory_fine - fine) as vadotar', 
                                            array('receipt_type' => array("ARC Finished Goods", "ARF Finished Goods", "ARC Refresh", "ARF Refresh")),
                                            array(), array('group_by' => 'receipt_type'));
-    $this->data['total_vadotar'] = $this->model->find('sum(((credit_weight + debit_weight) * (factory_purity - purity)) / 100 )   as vodator');
+    $this->data['total_vadotar'] = $this->model->find('sum(factory_fine - fine) as vodator');
 
     $this->data['company_vadotars'] = array('ARF' => 0, 'ARC' => 0);
     foreach ($company_vadotars as $company_vadotar) {
