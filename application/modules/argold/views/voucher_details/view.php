5fba92637b4f117067b18777786972c38e6de81a
kiran <kiran.indrale@ascratech.com>
2020-07-13 16:52:39 +0530

    change

--- application/modules/argold/views/voucher_details/view.php
+++ application/modules/argold/views/voucher_details/view.php
@@ -4,6 +4,11 @@
     <div class="form-group container">
       <p><h6>AC Name :<?=$record['account_name']?> </h6></p>
       <p><h6>Voucher No :<?=$record['voucher_number']?> </h6></p>
+      <p><h6>Item Name :<?=$record['narration']?> </h6></p>
+      <p><h6>Receipt Type :<?=$record['receipt_type']?></h6></p>
+      <?php if($record['receipt_type']=='Daily Drawer'){?>
+      <p><h6>Daily Drawer Type :<?=$record['dd_type']?></h6></p>
+      <?php }?>
     </div>
   </div>
   <div class="col-md-6">
@@ -11,12 +16,16 @@
     <div class="form-group container">
       <p><h6>Weight :<?=$record['debit_weight']?></h6></p>
       <p><h6>Melting :<?=$record['purity']?></h6></p>
-      <p><h6>Receipt Type :<?=$record['receipt_type']?></h6></p>
+      <p><h6>Fine :<?=$record['fine']?></h6></p>
+      <p><h6>Factory Purity :<?=$record['factory_purity']?></h6></p>
+      <p><h6>Factory Fine :<?=$record['factory_fine']?></h6></p>
+      <p><h6>Date : <?=date('d-m-Y',strtotime($record['created_at']))?></h6></p>
     </div>
   </div>
   
 </div>
 <hr class="">
+<h6 class="heading ">Metal Issue Voucher Details</h6>
 <?php
   $this->load->view('voucher_details/viewlist');
 ?>
