15d6ec2fd1608212c44bcd05c46427b08833746c
Atul Agrawal <atul@ascratech.com>
2020-07-19 13:38:53 +0530

    Tounch Loss Fine added in trial balance

--- application/modules/reports/views/ledgers/tr_issue.php
+++ application/modules/reports/views/ledgers/tr_issue.php
@@ -6,7 +6,7 @@
     <td><?= $record['account_name'];?></td>
   <?php endif; ?>
   <td><?= $record['str_voucher_date'] ?></td>
-  <td><?= $record['voucher_number'];?></td>
+  <td><?= $record['voucher_number'].' '.$record['narration'];?></td>
   <td class="text-right"><?= four_decimal($record['credit_weight']) ?></td>
   <td class="text-right"><?= four_decimal($record['purity']) ?></td>
   <td class="text-right"><?= four_decimal($record['credit_weight']*$record['purity']/100); ?></td>
