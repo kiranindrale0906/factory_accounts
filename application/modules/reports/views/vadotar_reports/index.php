debcb26eb1177ef182b35825d56f872010bd0bc4
Atul Agrawal <atul@ascratech.com>
2020-07-21 14:27:24 +0530

    changes

--- application/modules/reports/views/vadotar_reports/index.php
+++ application/modules/reports/views/vadotar_reports/index.php
@@ -11,7 +11,7 @@
           $companies = array('All', 'AR Gold', 'ARC', 'ARF');
           foreach ($companies as $index => $company) { ?>
             <a class="ml-5 <?= ($company_name== $company) ? 'bold black underline' : '' ?>" 
-               href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=<?= $detail ?>'><?= $company ?></a>
+               href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=<?= $group ?>'><?= $company ?></a>
           <?php }
         ?>
       </h5>
@@ -19,27 +19,35 @@
     <div class="form-group container"> 
       <h5> Select Period: 
         <a class="ml-5 <?= ($period=='date') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company_name?>&period=date&report_type=<?= $report_type ?>&detail=<?= $detail ?>'>Date</a>
+           href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company_name?>&period=date&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=<?= $group ?>'>Date</a>
         <a class="ml-5 <?= ($period=='week') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company_name?>&period=week&report_type=<?= $report_type ?>&detail=<?= $detail ?>'>Week</a>
+           href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company_name?>&period=week&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=<?= $group ?>'>Week</a>
         <a class="ml-5 <?= ($period=='month') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company_name?>&period=month&report_type=<?= $report_type ?>&detail=<?= $detail ?>'>Month</a>
+           href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company_name?>&period=month&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=<?= $group ?>'>Month</a>
       </h5>
     </div>
-    <div class="form-group container"> 
+    <!-- <div class="form-group container"> 
       <h5> Details: 
         <a class="ml-5 <?= ($detail=='yes') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company_name?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=yes'>Show Details</a>
+           href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company_name?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=yes&group=<?= $group ?>'>Show Details</a>
         <a class="ml-5 <?= ($detail=='no') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company_name?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=no'>Hide Details</a>
+           href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company_name?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=no&group=<?= $group ?>'>Hide Details</a>
+      </h5>
+    </div> -->
+    <div class="form-group container"> 
+      <h5> Group By: 
+        <a class="ml-5 <?= ($group=='') ? 'bold black underline' : '' ?>" 
+           href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company_name?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group='>None</a>
+        <a class="ml-5 <?= ($group=='voucher_date') ? 'bold black underline' : '' ?>" 
+           href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company_name?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=voucher_date'>Date</a>
       </h5>
     </div>
     <div class="form-group container"> 
       <h5> Report Type: 
         <a class="ml-5 <?= ($report_type=='vadotar') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company_name?>&period=<?= $period ?>&report_type=vadotar&detail=<?= $detail ?>'>Vadotar Report</a>
+           href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company_name?>&period=<?= $period ?>&report_type=vadotar&detail=<?= $detail ?>&group=<?= $group ?>'>Vadotar Report</a>
         <a class="ml-5 <?= ($report_type=='production') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company_name?>&period=<?= $period ?>&report_type=production&detail=<?= $detail ?>'>Production Report</a>
+           href='<?= base_url() ?>reports/vadotar_reports?company_name=<?= $company_name?>&period=<?= $period ?>&report_type=production&detail=<?= $detail ?>&group=<?= $group ?>'>Production Report</a>
       </h5>
     </div>
   </div>
