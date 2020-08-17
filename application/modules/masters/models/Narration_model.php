816d18d3097c0f9a13a14e24f6fbf77823238aed
kiran <kiran.indrale@ascratech.com>
2020-08-17 13:09:12 +0530

    change

--- application/modules/masters/models/Narration_model.php
+++ application/modules/masters/models/Narration_model.php
@@ -13,8 +13,9 @@ class Narration_model extends BaseModel {
   	return array(
 			array('field' => 'narrations[name]', 'label' => 'Name', 
             'rules' => array('trim','required',array('unique_department',
-                                            array($this, 'check_department_unique'))),
-            'errors'=> array('unique_department' => "The selected combination of name and purity already exist.")),
+                                            array($this, 'check_department_unique')),
+            array('category_special_charactor',array($this, 'check_special_charactor'))),
+            'errors'=> array('unique_department' => "The selected combination of name and purity already exist.",'category_special_charactor' => "Remove special charactor from category..")),
       array('field' => 'narrations[chain_purity]', 'label' => 'Purity', 
             'rules' => array('trim','required')),
       array('field' => 'narrations[chain_margin]', 'label' => 'Wastage', 
@@ -24,4 +25,12 @@ class Narration_model extends BaseModel {
     $fields = array('name', 'chain_purity');
     return parent::check_unique($fields);
   }
+  public function check_special_charactor($fields){
+     $charactors = preg_match('/[^a-zA-Z0-9/\d]/', $fields);
+     if($charactors!=0){
+     return false;
+     }else{
+      return true;
+     }
+   } 
 }
