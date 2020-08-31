096bbfe813588f62aa89244d81d5e8cc8a9de414
kiran <kiran.indrale@ascratech.com>
2020-08-31 21:10:08 +0530

    change

--- application/modules/transactions/migrations/20200831142558_add_refresh_details_table_in_transaction.php
+++ application/modules/transactions/migrations/20200831142558_add_refresh_details_table_in_transaction.php
@@ -6,20 +6,20 @@ class Migration_add_refresh_details_table_in_transaction extends CI_Model {
 
   public function up()
   {
-    $this->db->query("CREATE TABLE `refresh_details` (
-											  `id` int(11) NOT NULL,
-											  `refresh_id` int(11) NOT NULL,
-											  `weight` decimal(10,4) NOT NULL,
-											  `fine` decimal(10,4) NOT NULL,
-											  `purity` decimal(16,8) NOT NULL,
-											  `created_at` datetime NOT NULL,
-											  `updated_at` datetime NOT NULL,
-											  `created_by` int(11) NOT NULL,
-											  `updated_by` int(11) NOT NULL,
-											  `is_delete` tinyint(4) NOT NULL
-											) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
-    $this->db->query("ALTER TABLE `refresh_details` ADD PRIMARY KEY (`id`);");
-    $this->db->query("ALTER TABLE `refresh_details` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;");
+    // $this->db->query("CREATE TABLE `refresh_details` (
+				// 							  `id` int(11) NOT NULL,
+				// 							  `refresh_id` int(11) NOT NULL,
+				// 							  `weight` decimal(10,4) NOT NULL,
+				// 							  `fine` decimal(10,4) NOT NULL,
+				// 							  `purity` decimal(16,8) NOT NULL,
+				// 							  `created_at` datetime NOT NULL,
+				// 							  `updated_at` datetime NOT NULL,
+				// 							  `created_by` int(11) NOT NULL,
+				// 							  `updated_by` int(11) NOT NULL,
+				// 							  `is_delete` tinyint(4) NOT NULL
+				// 							) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
+    // $this->db->query("ALTER TABLE `refresh_details` ADD PRIMARY KEY (`id`);");
+    // $this->db->query("ALTER TABLE `refresh_details` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;");
   }
 
 
--- application/modules/transactions/migrations/20200831193007_add_factory_purity_and_factory_fine_in_refresh.php
+++ application/modules/transactions/migrations/20200831193007_add_factory_purity_and_factory_fine_in_refresh.php
@@ -6,9 +6,10 @@ class Migration_add_factory_purity_and_factory_fine_in_refresh extends CI_Model 
 
   public function up()
   {
-    $this->db->query("ALTER TABLE `refresh` ADD `factory_purity` DECIMAL(12,8) NOT NULL, ADD `factory_fine` DECIMAL(10,4) NOT NULL,ADD `metal_receipt_id` int(11) NOT NULL;");
-    $this->db->query("ALTER TABLE `refresh_details` ADD `factory_purity` DECIMAL(12,8) NOT NULL,
-    				 ADD `factory_fine` DECIMAL(10,4) NOT NULL;");
+  //   $this->db->query("ALTER TABLE `refresh` ADD `factory_purity` DECIMAL(12,8) NOT NULL, ADD `factory_fine` DECIMAL(10,4) NOT NULL,ADD `metal_receipt_id` int(11) NOT NULL;");
+  //   $this->db->query("ALTER TABLE `refresh_details` ADD `factory_purity` DECIMAL(12,8) NOT NULL,
+  //   				 ADD `factory_fine` DECIMAL(10,4) NOT NULL;");
+  // }
   }
 
 
