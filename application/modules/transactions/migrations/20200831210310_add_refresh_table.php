89be4b13d54457e0ce6677feb2b132fd611c1537
kiran <kiran.indrale@ascratech.com>
2020-08-31 21:14:10 +0530

    chnage

--- application/modules/transactions/migrations/20200831210310_add_refresh_table.php
+++ application/modules/transactions/migrations/20200831210310_add_refresh_table.php
@@ -6,8 +6,7 @@ class Migration_add_refresh_table extends CI_Model {
 
   public function up()
   {
-  	$this->db->query("drop table refresh_details");
-    $this->db->query("CREATE TABLE IF NOT EXISTS `refresh` (
+    $this->db->query("CREATE TABLE `refresh` (
 						  `id` int(11) NOT NULL AUTO_INCREMENT,
 						  `weight` decimal(16,8) NOT NULL,
 						  `factory_fine` decimal(16,8) NOT NULL,
@@ -21,7 +20,7 @@ class Migration_add_refresh_table extends CI_Model {
 						  `is_delete` int(11) NOT NULL DEFAULT 0,
 						  PRIMARY KEY (`id`)
 						) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;");
-    $this->db->query("CREATE TABLE IF NOT EXISTS `refresh_details` (
+    $this->db->query("CREATE TABLE  `refresh_details` (
 						  `id` int(11) NOT NULL AUTO_INCREMENT,
 						  `refresh_id` int(11) NOT NULL,
 						  `weight` decimal(16,8) NOT NULL,
