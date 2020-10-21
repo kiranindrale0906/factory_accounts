00872d3004f79bee9341c72c42494aae5ddb4f79
kiran <kiran.indrale@ascratech.com>
2020-10-21 19:35:09 +0530

    change

---
+++ application/modules/transactions/migrations/20201021183722_add_chitti_purity_in_ac_vouchers.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_add_chitti_purity_in_ac_vouchers extends CI_Model {
+
+  public function up()
+  {
+    // $this->db->query("");
+  }
+
+
+}
+
+?>
