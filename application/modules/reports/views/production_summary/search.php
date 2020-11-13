5366b8ddbb7ca68e85c3620532295f0571feb071
atul <atul@ascratech.com>
2020-11-13 12:46:24 +0530

    changes

--- application/modules/reports/views/production_summary/search.php
+++ application/modules/reports/views/production_summary/search.php
@@ -30,7 +30,7 @@
 
 <br />
 
-<div class="row"> 
+<!-- <div class="row"> 
   <div class="col-md-12">
     <h6>
       Category One:
@@ -42,7 +42,7 @@
       <?php } ?>
     </h6>
   </div>
-</div>
+</div> -->
 
 <br />
 
--- application/modules/reports/views/production_summary/tbody.php
+++ application/modules/reports/views/production_summary/tbody.php
@@ -4,6 +4,7 @@
     <td><?= $record['product_name'] ?></td>
     <td><?= $record['in_purity'] ?></td>
     <td><?= $record['out_purity'] ?></td>
+    <td><?= $record['account_name'] ?></td>
     <td><?= $record['category_one'] ?></td>
     <td><?= @$record['machine_size'] ?></td>
     <td><?= @$record['design_code'] ?></td>
