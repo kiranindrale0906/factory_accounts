200a90fbc3e322058275591a88cf4a14095e6b4e
atul <atul@ascratech.com>
2020-11-27 13:11:08 +0530

    changes

--- application/modules/reports/views/ledgers/tr_issue.php
+++ application/modules/reports/views/ledgers/tr_issue.php
@@ -6,7 +6,13 @@
     <td><?= $record['account_name'];?></td>
   <?php endif; ?>
   <td><?= $record['str_voucher_date'] ?></td>
-  <td><?= $record['voucher_number'].' '.$record['narration'].' '.$record['description'].' chitti '.$record['chitti_id'];?></td>
+  <td>
+    <?php
+      echo $record['voucher_number'].' '.$record['narration'].' '.$record['description'];
+      if ($record['voucher_type'] == 'metal issue voucher')
+        echo ' '.$record['chitti_no'];
+    ?>
+  </td>
   <td class="text-right"><?= four_decimal($record['credit_weight']) ?></td>
   <td class="text-right"><?= four_decimal($record['purity']) ?></td>
   <td class="text-right"><?= four_decimal($record['credit_weight']*$record['purity']/100); ?></td>
