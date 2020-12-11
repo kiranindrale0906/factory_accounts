b956d8e2f65336a05ad75e2450f02fd770da2c1a
kiran <kiran.indrale@ascratech.com>
2020-12-11 17:09:58 +0530

    chaneg

--- application/modules/argold/helpers/client_metal_receipt_vouchers_helper.php
+++ application/modules/argold/helpers/client_metal_receipt_vouchers_helper.php
@@ -70,7 +70,7 @@ if (!function_exists('get_row_actions')) {
                                      'url' => ADMIN_PATH.'argold/metal_receipt_gold_rates/edit/'.$row['id'],
                                      'confirm_message' => "",
                                      'class' => 'text-warning text-uppercase');
-    if ($row['account_name'] == 'SWARN SHILP CHAINS AND JEWELLERS PVT LTD')
+    // if ($row['account_name'] == 'SWARN SHILP CHAINS AND JEWELLERS PVT LTD')
       $actions["Edit"] =  array('request' => "http", 
                                      'url' => ADMIN_PATH.'argold/metal_issue_account_names/edit/'.$row['id'],
                                      'confirm_message' => "",
