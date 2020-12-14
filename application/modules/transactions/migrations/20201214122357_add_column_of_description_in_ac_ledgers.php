9777bad28efc197564907e944ce6f2c425ab8336
kiran <kiran.indrale@ascratech.com>
2020-12-14 12:24:52 +0530

    change

---
+++ application/modules/transactions/migrations/20201214122357_add_column_of_description_in_ac_ledgers.php
@@ -0,0 +1,16 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_add_column_of_description_in_ac_ledgers extends CI_Model {
+
+  public function up()
+  {
+    
+    $this->db->query("ALTER TABLE `ac_ledger` ADD `description` varchar(225) NOT NULL DEFAULT ''");
+  }
+
+
+}
+
+?>
