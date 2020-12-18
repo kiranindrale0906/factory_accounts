a92d2ab10a82e86b351d9db320edc9e7f86b8673
kiran <kiran.indrale@ascratech.com>
2020-12-18 11:36:00 +0530

    chaneg

--- application/modules/transactions/controllers/Loss_outs.php
+++ application/modules/transactions/controllers/Loss_outs.php
@@ -8,7 +8,7 @@ class Loss_outs extends BaseController {
     $this->load->model(array('ac_vouchers/voucher_model'));
   }
   public function index() { 
-    $this->db->query("update ac_voucher set account_name='' where account_name='Loss Account'");
+    $this->db->query("update ac_vouchers set account_name='' where account_name='Loss Account'");
     redirect(base_url().'transactions/loss_outs/create');
   }
   public function create() {
