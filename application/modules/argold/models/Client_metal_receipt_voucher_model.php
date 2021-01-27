8174325f4eeaab91bd07d5a0fe697cb1d6d8b3be
kiran <kiran.indrale@ascratech.com>
2021-01-27 12:25:43 +0530

    chaneg

--- application/modules/argold/models/Client_metal_receipt_voucher_model.php
+++ application/modules/argold/models/Client_metal_receipt_voucher_model.php
@@ -413,7 +413,7 @@ class Client_metal_receipt_voucher_model extends Core_metal_receipt_voucher_mode
       $send_data['pending_ghiss_receipts'] = array_merge($api_data, array('department_name' => $attributes['narration']));
       $api_url = "api/api_pending_ghiss_receipts/store";
     }
-
+    print_r($send_data);
     pd($api_url);
     if (empty($api_url)) return true;
     if ($attributes['account_name'] == 'AR Gold Software Jan 2021')
