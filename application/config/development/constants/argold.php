350d992391f8bbe4f8fd7b8dda97001f1223624f
kiran <kiran.indrale@ascratech.com>
2021-01-27 11:09:41 +0530

    change

--- application/config/development/constants/argold.php
+++ application/config/development/constants/argold.php
@@ -4,9 +4,9 @@
   define('API_ARF_BASE_PATH', 'http://localhost/arf/');
   define('API_ARC_BASE_PATH', 'http://localhost/arc/');
 
-  define('API_ARG_NOV_2020', 'https://staging-argold.ascratech.com/');
-  define('API_ARC_NOV_2020', 'http://localhost/arf/');
-  define('API_ARF_NOV_2020', 'http://localhost/arc/');
+  define('API_ARG_NOV_2020_PATH', 'https://staging-argold.ascratech.com/');
+  define('API_ARC_NOV_2020_PATH', 'http://localhost/arf/');
+  define('API_ARF_NOV_2020_PATH', 'http://localhost/arc/');
   define('API_ARG_JAN_2021', 'https://staging-argold.ascratech.com/');
   define('API_ARC_JAN_2021', 'http://localhost/arf/');
   define('API_ARF_JAN_2021', 'http://localhost/arc/');
