f3341697ba3edfa5e61cf425ce7f5083f032fbdd
kiran <kiran.indrale@ascratech.com>
2021-01-27 13:52:29 +0530

    change

--- application/modules/argold/models/Client_metal_issue_voucher_model.php
+++ application/modules/argold/models/Client_metal_issue_voucher_model.php
@@ -69,10 +69,10 @@ class Client_metal_issue_voucher_model extends Core_metal_issue_voucher_model {
   }
 
   public function after_save($action) {
+    pd('hiihiihi');
     parent::after_save($action);
     //$this->create_metal_receipt_voucher_for_finished_goods();
     $account_name=trim($this->attributes['account_name']);
-    pd($account_name);
     if (   ENABLE_API_FOR_RECEIPT 
         && $this->attributes['receipt_type'] != 'Internal' 
         && (   $account_name == 'AR Gold Software Nov 2020'
