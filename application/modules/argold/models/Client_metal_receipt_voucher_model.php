70b3872dcc2dd251f51c7661839ea22ca5321723
kiran <kiran.indrale@ascratech.com>
2021-01-27 11:20:09 +0530

    change

--- application/modules/argold/models/Client_metal_receipt_voucher_model.php
+++ application/modules/argold/models/Client_metal_receipt_voucher_model.php
@@ -249,6 +249,7 @@ class Client_metal_receipt_voucher_model extends Core_metal_receipt_voucher_mode
   }
 
   private function set_receipt_type_for_all_metal_issue_vouchers() {
+    pd($this->formdata['metal_issue_vouchers']);
     if (isset($this->formdata['metal_issue_vouchers'])) {
       foreach ($this->formdata['metal_issue_vouchers'] as $index => $metal_issue_voucher) {
         $this->formdata['metal_issue_vouchers'][$index]['receipt_type'] = $this->attributes['receipt_type'];
