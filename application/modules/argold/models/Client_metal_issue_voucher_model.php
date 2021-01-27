6bf3ab1b8156dd3a63eafe3bed4e20ca9adce15c
kiran <kiran.indrale@ascratech.com>
2021-01-27 11:17:17 +0530

    chaneg

--- application/modules/argold/models/Client_metal_issue_voucher_model.php
+++ application/modules/argold/models/Client_metal_issue_voucher_model.php
@@ -80,8 +80,7 @@ class Client_metal_issue_voucher_model extends Core_metal_issue_voucher_model {
             || $account_name == 'AR Gold Software Jan 2021'
             || $account_name == 'ARF Software Jan 2021'
             || $account_name == 'ARC Software Jan 2021')){
-      print_r($account_name);
-      pd($this->attributes['receipt_type']);
+      pd($this->attributes);
       $this->client_metal_receipt_voucher_model->send_request_to_factory($this->attributes);
     }
             // || $this->attributes['receipt_type'] == 'ARF Chain Receipt'
