98a9164a5c4a9cf813dd264c6813c2b746f630ff
kiran <kiran.indrale@ascratech.com>
2021-02-05 19:44:08 +0530

    change

--- application/modules/reports/views/trial_balances/index.php
+++ application/modules/reports/views/trial_balances/index.php
@@ -3,9 +3,6 @@
   $this->load->view('reports/ledgers/report_header', array('header' => 'Trial Balance')); 
 ?>
 
-<div class="col-md-12">
-  <?= getHttpButton('Update Vadotar / Vatav', base_url().'reports/trial_balances?update_vadotar=1', 'float-right btn-success ml-5'); ?>
-</div>
 
 <?php $this->load->view('trial_balances/trial_balance', array('trial_balance_records' => $trial_balance, 'loss_account' => 0)); ?>
 
@@ -13,6 +10,9 @@
 <h5 class="ml-2 pl-2">Loss Account Details</h5>
 <?php $this->load->view('trial_balances/trial_balance', array('trial_balance_records' => $loss_account_records, 'loss_account' => 1)); ?>
 <hr />
+<div class="col-md-12">
+  <?= getHttpButton('Update Vadotar / Vatav', base_url().'reports/trial_balances?update_vadotar=1', 'float-right btn-success ml-5'); ?>
+</div>
 <div class="row">
 <?php $this->load->view('trial_balances/alloy_vodator_balance'); ?>
 <?php $this->load->view('trial_balances/gpc_vodator_balance'); ?>
