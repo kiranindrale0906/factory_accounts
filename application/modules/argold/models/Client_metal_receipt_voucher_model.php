1d2f9cd09ef8187961ac1194dd4c20495aaaf569
Ubuntu <ubuntu@ip-172-26-8-254.ap-southeast-1.compute.internal>
2021-02-05 19:16:13 +0530

    changes

--- application/modules/argold/models/Client_metal_receipt_voucher_model.php
+++ application/modules/argold/models/Client_metal_receipt_voucher_model.php
@@ -495,7 +495,7 @@ class Client_metal_receipt_voucher_model extends Core_metal_receipt_voucher_mode
                   'site_name' => $site_name.' '.$hostversion);
       $data['id'] = '';
       if (!empty($metal_receipt_voucher)) $data['id'] = $metal_receipt_voucher['id'];
-        
+      if (empty($record['weight'])) return;
       if(empty($metal_receipt_voucher['debit_weight'])
          || ($metal_receipt_voucher['debit_weight'] != $record['weight'])) {
         $metal_receipt_obj = new metal_receipt_voucher_model($data);
