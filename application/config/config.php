c15afb6bd7970e6a7f92db1da0ce420aeec2f8ba
kiran <kiran.indrale@ascratech.com>
2021-02-08 18:19:53 +0530

    change

--- application/config/config.php
+++ application/config/config.php
@@ -32,6 +32,12 @@ $ark_root           = $protocol . "://" . $_SERVER['HTTP_HOST'];
 $ark_root           .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 
 $config['base_url'] = $ark_root;
+define('API_ARG_JAN2021_PATH', 'https://staging-argold.ascratech.com/');
+define('API_ARF_JAN2021_PATH', 'https://staging-arf.ascratech.com/');
+define('API_ARC_JAN2021_PATH', 'https://staging-arc.ascratech.com/');
+define('API_ARG_NOV2020_PATH', 'http://staging-argold.ascratech.com/');
+define('API_ARC_NOV2020_PATH', 'http://staging-arc.ascratech.com/');
+define('API_ARF_NOV2020_PATH', 'http://staging-arf.ascratech.com/');
 
 
 /*
