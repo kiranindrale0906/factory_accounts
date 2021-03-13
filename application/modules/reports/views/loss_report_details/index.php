266edad803e9da55c69f43046f1d879f7fe58efb
kiran <kiran.indrale@ascratech.com>
2021-03-13 14:28:02 +0530

    chang

--- application/modules/reports/views/loss_report_details/index.php
+++ application/modules/reports/views/loss_report_details/index.php
@@ -28,41 +28,41 @@
     <?php 
       $sum_weight=$sum_fine=$sum_factory_fine=$sum_receipt_weight=$sum_receipt_weight=$sum_total_fine=$sum_receipt_fine=$sum_loss_befor_recovery=$sum_melting_production=$sum_production=$sum_after_recovery=$sum_after_fine=$sum_loss=$sum_unrecoverable=0;
      foreach ($loss_details as $index => $loss_out_detail) {
-      $sum_weight+=$loss_out_detail->in_weight;
-      $sum_fine+=($loss_out_detail->in_weight*$loss_out_detail->in_lot_purity/100);
-      $sum_melting_production+=($loss_out_detail->out_weight);
-      $sum_production+=($loss_out_detail->production);
-      $sum_after_recovery+=($loss_out_detail->after_recovery);
-      $sum_unrecoverable+=($loss_out_detail->unrecovery);
-      $sum_after_fine+=($loss_out_detail->fine);
-      $sum_loss+=(($loss_out_detail->in_weight*$loss_out_detail->in_lot_purity/100)-($loss_out_detail->fine)-($loss_out_detail->unrecovery));
-      $sum_loss_befor_recovery+=!empty($loss_out_detail->out_weight)?(($loss_out_detail->in_weight*$loss_out_detail->in_lot_purity/100)/$loss_out_detail->out_weight):0;
+      $sum_weight+=$loss_out_detail['in_weight'];
+      $sum_fine+=($loss_out_detail['in_weight']*$loss_out_detail['in_lot_purity']/100);
+      $sum_melting_production+=($loss_out_detail['out_weight']);
+      $sum_production+=($loss_out_detail['production']);
+      $sum_after_recovery+=($loss_out_detail['after_recovery']);
+      $sum_unrecoverable+=($loss_out_detail['unrecovery']);
+      $sum_after_fine+=($loss_out_detail['fine']);
+      $sum_loss+=(($loss_out_detail['in_weight']*$loss_out_detail['in_lot_purity']/100)-($loss_out_detail['fine'])-($loss_out_detail['unrecovery']));
+      $sum_loss_befor_recovery+=!empty($loss_out_detail['out_weight'])?(($loss_out_detail['in_weight']*$loss_out_detail['in_lot_purity']/100)/$loss_out_detail['out_weight']):0;
       ?>
       <tr>
-        <td><?=date('d-m-Y',strtotime($loss_out_detail->created_at));?></td>
-        <td><?=date('d-m-Y',strtotime($loss_out_detail->first_date)).' To '.date('d-m-Y',strtotime($loss_out_detail->last_date)); ?></td>
-        <td class="text-right"><?=four_decimal($loss_out_detail->in_weight)?></td>
-        <td class="text-right"><?=four_decimal($loss_out_detail->in_lot_purity)?></td>
-        <td class="text-right"><?=$fine=four_decimal($loss_out_detail->in_weight*$loss_out_detail->in_lot_purity/100);?></td>
-        <td class="text-right"><?=four_decimal($loss_out_detail->out_weight);?></td>
-        <td class="text-right"><?=!empty($loss_out_detail->out_weight)?eight_decimal($fine/$loss_out_detail->out_weight):0;?></td>
-        <td class="text-right"><?=four_decimal($loss_out_detail->production);?></td>
+        <td><?=date('d-m-Y',strtotime($loss_out_detail['created_at']));?></td>
+        <td><?=date('d-m-Y',strtotime($loss_out_detail['first_date'])).' To '.date('d-m-Y',strtotime($loss_out_detail['last_date'])); ?></td>
+        <td class="text-right"><?=four_decimal($loss_out_detail['in_weight'])?></td>
+        <td class="text-right"><?=four_decimal($loss_out_detail['in_lot_purity'])?></td>
+        <td class="text-right"><?=$fine=four_decimal($loss_out_detail['in_weight']*$loss_out_detail['in_lot_purity']/100);?></td>
+        <td class="text-right"><?=four_decimal($loss_out_detail['out_weight']);?></td>
+        <td class="text-right"><?=!empty($loss_out_detail['out_weight'])?eight_decimal($fine/$loss_out_detail['out_weight']):0;?></td>
+        <td class="text-right"><?=four_decimal($loss_out_detail['production']);?></td>
         <td class="text-right">
-          <a href=<?= base_url()."ac_vouchers/voucher_listing?parent_id=".$loss_out_detail->parent_id ?> target='_blank'><?=four_decimal($loss_out_detail->after_recovery);?></a></td>
-        <td class="text-right"><?=four_decimal($loss_out_detail->purity);?></td>
-        <td class="text-right"><?=four_decimal($loss_out_detail->fine);?></td>
-        <td class="text-right"><?=$loss=four_decimal($fine-$loss_out_detail->fine-$loss_out_detail->unrecovery);?></td>
-        <td class="text-right"><?=!empty($loss_out_detail->unrecovery)?eight_decimal(-$loss_out_detail->unrecovery):0;?></td>
-        <td class="text-right"><?=!empty($loss_out_detail->out_weight)?eight_decimal($loss/$loss_out_detail->out_weight):0;?></td>
-        <td class="text-right"><?=!empty($loss_out_detail->production)?four_decimal($loss/$loss_out_detail->production):0;?></td>
+          <a href=<?= base_url()."ac_vouchers/voucher_listing?parent_id=".$loss_out_detail['parent_id'] ?> target='_blank'><?=four_decimal($loss_out_detail['after_recovery']);?></a></td>
+        <td class="text-right"><?=four_decimal($loss_out_detail['purity']);?></td>
+        <td class="text-right"><?=four_decimal($loss_out_detail['fine']);?></td>
+        <td class="text-right"><?=$loss=four_decimal($fine-$loss_out_detail['fine']-$loss_out_detail['unrecovery']);?></td>
+        <td class="text-right"><?=!empty($loss_out_detail['unrecovery'])?eight_decimal(-$loss_out_detail['unrecovery']):0;?></td>
+        <td class="text-right"><?=!empty($loss_out_detail['out_weight'])?eight_decimal($loss/$loss_out_detail['out_weight']):0;?></td>
+        <td class="text-right"><?=!empty($loss_out_detail['production'])?four_decimal($loss/$loss_out_detail['production']):0;?></td>
         <td class="text-right">
         <?php //if($parent_id==0){ ?>
-          <a href=<?= base_url()."transactions/metal_receipt_vouchers?receipt_type=Metal&parent_id=".$loss_out_detail->parent_id ?> target='_blank'>create metal receipt</a>
+          <a href=<?= base_url()."transactions/metal_receipt_vouchers?receipt_type=Metal&parent_id=".$loss_out_detail['parent_id'] ?> target='_blank'>create metal receipt</a>
           <?php //}?>
         </td>
         <td class="text-right">
         <?php if(empty($loss_out_detail->unrecovery)){ ?>
-          <a href=<?= base_url()."argold/unrecovarable_account_records/store?from=view&account_name=Unrecovarable&credit_weight=".$loss."&narration=".urlencode($category)."&parent_id=".$loss_out_detail->parent_id ?> target='_blank'>Unrecovarable</a>
+          <a href=<?= base_url()."argold/unrecovarable_account_records/store?from=view&account_name=Unrecovarable&credit_weight=".$loss."&narration=".urlencode($category)."&parent_id=".$loss_out_detail['parent_id'] ?> target='_blank'>Unrecovarable</a>
           <?php }?>
         </td>
       </tr>
