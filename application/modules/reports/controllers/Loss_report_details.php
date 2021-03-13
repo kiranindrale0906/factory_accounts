67fc4c59d0b8fb4d3e7bd3a59f0a6a32b0983e85
kiran <kiran.indrale@ascratech.com>
2021-03-13 13:59:22 +0530

    chaneg

--- application/modules/reports/controllers/Loss_report_details.php
+++ application/modules/reports/controllers/Loss_report_details.php
@@ -47,19 +47,19 @@ class Loss_report_details extends Ledgers {
   private function factory_wise_record_array($records){
 
     if(!empty($records)){
-       foreach ($records->data->loss_details->loss_detail as $index => $loss_detail) {
+       foreach ($records->data->loss_details->loss_detail as $index => $loss_data) {
          $where['purity != factory_purity'] = NULL;
          $where['account_name != '] = 'VADOTAR';
-         $records->data->loss_details->loss_detail->[$index]->production=0;
-         if(!empty($loss_detail->first_date)){
-            $where['date(voucher_date) >='] = date('Y-m-d',strtotime($loss_detail->first_date));
+         $records->data->loss_details->loss_detail->$index->production=0;
+         if(!empty($loss_data->first_date)){
+            $where['date(voucher_date) >='] = date('Y-m-d',strtotime($loss_data->first_date));
          }
-         if(!empty($loss_detail->last_date)){
-            $where['date(voucher_date) <='] = date('Y-m-d',strtotime($loss_detail->last_date));
+         if(!empty($loss_data->last_date)){
+            $where['date(voucher_date) <='] = date('Y-m-d',strtotime($loss_data->last_date));
           }
           $product_production= $this->ledger_model->find('-1*sum(credit_weight-debit_weight) as weight',$where);
-          $loss_account_details= $this->voucher_model->find('sum(debit_weight) as weight,factory_purity,sum(fine) as fine',array('parent_id'=>$loss_detail->parent_id,'account_name!='=>'Unrecovarable'));
-          $unrecovery_details= $this->voucher_model->find('sum(credit_weight) as weight',array('parent_id'=>$loss_detail->parent_id,'account_name'=>'Unrecovarable'));
+          $loss_account_details= $this->voucher_model->find('sum(debit_weight) as weight,factory_purity,sum(fine) as fine',array('parent_id'=>$loss_data->parent_id,'account_name!='=>'Unrecovarable'));
+          $unrecovery_details= $this->voucher_model->find('sum(credit_weight) as weight',array('parent_id'=>$loss_data->parent_id,'account_name'=>'Unrecovarable'));
           $records->data->loss_details->loss_detail->$index->production=$product_production['weight'];
           $records->data->loss_details->loss_detail->$index->after_recovery=!empty($loss_account_details)?$loss_account_details['weight']:0;
           $records->data->loss_details->loss_detail->$index->unrecovery=!empty($unrecovery_details)?$unrecovery_details['weight']:0;
