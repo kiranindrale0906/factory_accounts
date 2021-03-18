99d5375c5bc1a0a30a8c0e9e988431628f25b43c
kiran <kiran.indrale@ascratech.com>
2021-03-18 19:49:20 +0530

    chaneg

--- application/modules/reports/controllers/Loss_report_details.php
+++ application/modules/reports/controllers/Loss_report_details.php
@@ -59,6 +59,7 @@ class Loss_report_details extends Ledgers {
   }
 
   private function factory_wise_record_array($loss_details){
+    pd($loss_details);
     $factory_wise_record=array();
     if(!empty($loss_details)){
        foreach ($loss_details as $index => $loss_data) {
@@ -74,7 +75,7 @@ class Loss_report_details extends Ledgers {
           }
           $product_production= $this->ledger_model->find('-1*sum(credit_weight-debit_weight) as weight',$where);
           $loss_account_details= $this->voucher_model->find('sum(debit_weight) as weight,factory_purity,sum(fine) as fine',array('parent_id'=>$loss_data['parent_id'],'account_name!='=>'Unrecovarable'));
-          
+
           $unrecovery_details= $this->voucher_model->find('sum(credit_weight) as weight',array('parent_id'=>$loss_data['parent_id'],'account_name'=>'Unrecovarable'));
 
           $factory_wise_record[$index]['production']=$product_production['weight'];
