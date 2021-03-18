93f88377786c4f8f005a47b0ccc23dac6d95e6a2
kiran <kiran.indrale@ascratech.com>
2021-03-18 20:25:07 +0530

    chaneg

--- application/modules/transactions/models/Transfer_voucher_model.php
+++ application/modules/transactions/models/Transfer_voucher_model.php
@@ -24,12 +24,7 @@ class Transfer_voucher_model extends BaseModel {
 
   public function save($action=true) {
     $account_id=$this->account_model->find('id',array('name'=>$this->attributes['account_name']))['id'];
-    $voucher_id=$this->voucher_model->get('id',array('account_id'=>$account_id,'date(created_at) >='=>date('Y-m-d',strtotime($this->attributes['from_date'])),'date(created_at) <='=>date('Y-m-d',strtotime($this->attributes['to_date']))));
-    $voucher_ids=array_column($voucher_id, 'id');
-    $voucher_ids=implode(',', $voucher_ids);
-
-
-
+    
     $this->db->query("insert into ".NEW_DB_NAME.".ac_vouchers select * from ".OLD_DB_NAME.".ac_vouchers where account_id = ".$account_id." and date(created_at) >= '".date('Y-m-d',strtotime($this->attributes['from_date']))."' and date(created_at) <= '".date('Y-m-d',strtotime($this->attributes['to_date']))."'");
 
 
