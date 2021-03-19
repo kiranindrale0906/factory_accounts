7333298012d34249d6a549b057ad348ede570412
kiran <kiran.indrale@ascratech.com>
2021-03-19 12:40:19 +0530

    chaneg

--- application/modules/reports/views/loss_accounts/form.php
+++ application/modules/reports/views/loss_accounts/form.php
@@ -10,7 +10,7 @@
     <?php 
       $sum_weight=$sum_fine=0;
      foreach ($loss_categories as $index => $loss_category) {
-      $sum_fine+=$loss_category['fine'];
+      $sum_fine+=four_decimal($loss_category['fine']);
       ?>
       <tr>
         <td class=""><a href="<?=base_url()?>reports/loss_account_details?category=<?=$index ?>"><?=$index?></a></td>
