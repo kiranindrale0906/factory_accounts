12d5e585a954065058d4d4ab5f7dd395e143fc03
kiran <kiran.indrale@ascratech.com>
2021-03-19 12:31:58 +0530

    chaneg

--- application/modules/reports/controllers/Loss_reports.php
+++ application/modules/reports/controllers/Loss_reports.php
@@ -32,6 +32,7 @@ class Loss_reports extends BaseController {
         $jan2021_records=json_decode(curl_post_request($url,$data),true);
         $jan2021_records=!empty($jan2021_records)?$jan2021_records['data']['loss_details']['loss_detail']:$jan2021_records['data']['loss_details']['loss_detail']=array();
         $ghiss_melting_loss=$this->voucher_model->get('description,site_name,credit_weight as in_weight,purity as in_lot_purity,argold_id as parent_id,0 as out_weight', array('account_name'=>'Loss Account','site_name'=>'ARC Jan 2021','receipt_type'=>'Ghiss Melting Loss'),array());
+        pd($jan2021_records);
         $arg_jan2021_records=array_merge($jan2021_records,$ghiss_melting_loss);
 
       }elseif(isset($_GET['site_name'])&&$_GET['site_name']=='ARF'){
