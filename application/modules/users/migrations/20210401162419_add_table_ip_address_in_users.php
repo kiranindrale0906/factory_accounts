ebf2357198deb2f38dc5fa53ea45a3d9eb258cf9
kiran <kiran.indrale@ascratech.com>
2021-04-01 17:26:19 +0530

    chaneg

--- application/modules/users/migrations/20210401162419_add_table_ip_address_in_users.php
+++ application/modules/users/migrations/20210401162419_add_table_ip_address_in_users.php
@@ -6,7 +6,16 @@ class Migration_add_table_ip_address_in_users extends CI_Model {
 
   public function up()
   {
-    $this->db->query("");
+     $this->db->query("CREATE TABLE IF NOT EXISTS `ip_addresses` (
+						   `id` int(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+						  `ip_address` varchar(100) NOT NULL,
+						  `user_id` int(11) NOT NULL,
+						  `created_by` int(11) DEFAULT NULL,
+						  `updated_by` int(11) DEFAULT NULL,
+						  `created_at` datetime NOT NULL,
+						  `updated_at` datetime NOT NULL,
+						  `is_delete` int(4) NOT NULL
+						);");
   }
 
 
