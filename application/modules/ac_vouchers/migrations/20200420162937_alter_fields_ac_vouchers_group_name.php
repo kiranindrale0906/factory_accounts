a135a2da1d3751566cbc8e0687fbf6bd3b16f24f
kiran <kiran.indrale@ascratech.com>
2021-04-01 17:21:03 +0530

    chaneg

--- application/modules/ac_vouchers/migrations/20200420162937_alter_fields_ac_vouchers_group_name.php
+++ application/modules/ac_vouchers/migrations/20200420162937_alter_fields_ac_vouchers_group_name.php
@@ -6,8 +6,8 @@ class Migration_alter_fields_ac_vouchers_group_name extends CI_Model {
 
   public function up()
   {
-  	$sql = "ALTER TABLE `ac_vouchers` ADD `from_group_name` varchar(225),add `to_group_name` varchar(225),add `from_group_id` int(11),add `to_group_id` int(11);";
-    $this->db->query($sql);
+  	// $sql = "ALTER TABLE `ac_vouchers` ADD `from_group_name` varchar(225),add `to_group_name` varchar(225),add `from_group_id` int(11),add `to_group_id` int(11);";
+   //  $this->db->query($sql);
   }
 
 
