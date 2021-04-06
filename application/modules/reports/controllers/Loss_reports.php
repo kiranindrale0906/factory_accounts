6a1efa520511bee6badab81d95a441cb49fa5712
kiran <kiran.indrale@ascratech.com>
2021-04-06 12:45:03 +0530

    chaeg

--- application/modules/reports/controllers/Loss_reports.php
+++ application/modules/reports/controllers/Loss_reports.php
@@ -23,7 +23,9 @@ class Loss_reports extends BaseController {
     $categories= $this->voucher_model->get('description', array('account_name'=>'Loss Account','parent_id'=>0,'date(created_at)>='=>'2021-03-13'),array(),array('group_by'=>'description'));
     $loss_details= $this->voucher_model->get('description,fine,id', array('account_name'=>'Loss Account','parent_id'=>0),array());
     $category_names=array_column($categories,'description');
-    
+    $category_names=array('Bengali Loss','Buffing Loss','Hammering Loss','Machine Room Loss','Melting Loss','Office Loss','Outside Ball Making Loss','Pasta Loss','Shampoo And Steel Loss','Sisma Machine Room',
+      'Solder','Tarpatta And Flatting Loss','Walnut Loss','Walnut Shampoo Loss');
+
     foreach ($category_names as $category_name_index => $category_name_value) {
       $data['department_name']=$category_name_value;
       if(isset($_GET['site_name'])&&$_GET['site_name']=='ARC'){
@@ -54,31 +56,38 @@ class Loss_reports extends BaseController {
         $arg_jan2021_records=array_merge($records,$ghiss_melting_loss);
       }
       if(!empty($arg_jan2021_records)){
-        $total_production=$total_loss_fine=$recoverd_loss_fine=$all_loss_before_recovery=$unrecovery_loss=$total_product_production=0;
+        $total_production=$total_loss_fine=$recoverd_loss_fine=$all_loss_before_recovery=$unrecovery_loss=$fine_loss=$total_out_weight=$per_kg_loss=$total_per_kg_loss=$before_recovery_loss=$total_before_recovery_loss=$recovered_loss=$total_recovery_loss=$after_recovery_loss=$total_after_recovery_loss=$total_unrecovery_loss=$total_balance=$balance=0;
         foreach ($arg_jan2021_records as $index => $arg_loss_detail) {
           if($category_name_value==$arg_loss_detail['description']){
             $factory_wise_record[$index]['production']=0;
             $loss_account_details= $this->voucher_model->find('sum(debit_weight) as weight,factory_purity,sum(fine) as fine',array('parent_id'=>$arg_loss_detail['parent_id'],'account_name!='=>'Unrecovarable'));
             
             $unrecovery_details = $this->voucher_model->find('sum(credit_weight) as weight',array('parent_id'=>$arg_loss_detail['parent_id'],'account_name'=>'Unrecovarable'));
-         
-            $total_production+=$arg_loss_detail['out_weight'];
-            $unrecovery_loss+=!empty($unrecovery_details)?$unrecovery_details['weight']:0;
-            $total_loss_fine+=($arg_loss_detail['in_weight']*$arg_loss_detail['in_lot_purity']/100);
-            $all_loss_before_recovery+=!empty($arg_loss_detail['out_weight'])?(($arg_loss_detail['in_weight']*$arg_loss_detail['in_lot_purity']/100)/$arg_loss_detail['out_weight']*1000):0;
-            $recoverd_loss_fine+=($arg_loss_detail['in_weight']*$arg_loss_detail['in_lot_purity']/100)-$loss_account_details['fine']-$unrecovery_details['weight'];
-            // $total_product_production+=$product_production['weight'];
-            // $where['purity != factory_purity'] = NULL;
-            // $where['account_name != '] = 'VADOTAR';
-            // $where['(year(voucher_date)- month(voucher_date)) <']=date("Y-m");
 
-          // $product_production= $this->ledger_model->find('-1*sum(credit_weight-debit_weight) as weight',$where);
-          $this->data['loss_categories'][$category_name_value]['out_weight']=$total_production;
-          $this->data['loss_categories'][$category_name_value]['overall_loss_fine']=$total_loss_fine;
-          $this->data['loss_categories'][$category_name_value]['recoverd_loss_fine']=$recoverd_loss_fine;
-          $this->data['loss_categories'][$category_name_value]['all_loss_before_recovery']=($all_loss_before_recovery);
-          $this->data['loss_categories'][$category_name_value]['unrecovery_loss']=($unrecovery_loss);
-          $this->data['loss_categories'][$category_name_value]['all_loss_after_recovery']=!empty($total_production)?($total_loss_fine/$total_production*1000):0;
+            $fine_loss=($arg_loss_detail['in_weight']*$arg_loss_detail['in_lot_purity']/100);
+            $per_kg_loss=($fine_loss/$arg_loss_detail['out_weight']*1000);
+            $before_recovery_loss=($fine_loss/$arg_loss_detail['out_weight']*100);
+            $recovered_loss=($fine_loss-$loss_account_details['fine']-$unrecovery_details['weight']);
+            $after_recovery_loss=($fine_loss-$recovered_loss/$arg_loss_detail['out_weight']*100);
+            $unrecovery_loss=!empty($unrecovery_details)?$unrecovery_details['weight']:0;
+            $balance=$fine_loss-$recovered_loss-$unrecovery_loss;
+         
+            $total_out_weight+=$arg_loss_detail['out_weight'];
+            $total_loss_fine+=$fine_loss;
+            $total_per_kg_loss+=$per_kg_loss;
+            $total_before_recovery_loss+=$before_recovery_loss;
+            $total_recovery_loss+=$recovered_loss;
+            $total_after_recovery_loss+=$after_recovery_loss;
+            $total_unrecovery_loss+=$unrecovery_loss;
+            $total_balance+=$balance;
+          $this->data['loss_categories'][$category_name_value]['loss_fine']=$total_loss_fine;
+          $this->data['loss_categories'][$category_name_value]['out_weight']=$total_out_weight;
+          $this->data['loss_categories'][$category_name_value]['per_kg_loss']=$total_per_kg_loss;
+          $this->data['loss_categories'][$category_name_value]['before_recovery_loss']=$total_before_recovery_loss;
+          $this->data['loss_categories'][$category_name_value]['recoverd_loss_fine']=$total_recovery_loss;
+          $this->data['loss_categories'][$category_name_value]['after_recovery_loss']=$total_after_recovery_loss;
+          $this->data['loss_categories'][$category_name_value]['unrecoverable_loss']=$total_unrecovery_loss;
+          $this->data['loss_categories'][$category_name_value]['balance']=$total_balance;
           }
         }
       }
--- application/modules/reports/views/loss_reports/index.php
+++ application/modules/reports/views/loss_reports/index.php
@@ -27,46 +27,50 @@
         <th class="text-right">Fine Loss</th>
         <th class="text-right">Total Out Weight</th>
         <th class="text-right">Per Kg Loss</th>
-        <th class="text-right">Recovered Loss</th>
         <th class="text-right">% Loss Before Recovery</th>
+        <th class="text-right">Recovered Loss</th>
         <th class="text-right">% Loss After Recovery</th>
         <th class="text-right">Unrecoverable Loss</th>
+        <th class="text-right">Balance Loss</th>
         </tr>
     </thead>
     <tbody>
     <?php 
-      $sum_overall_loss_fine=$sum_all_loss_before_recovery=$sum_per_kg_loss=$sum_recoverd_loss_fine=$sum_out_weight=$sum_fine=$sum_unrecovery_loss=$sum_all_loss_after_recovery=$sum_out_weight=0;
+      $sum_loss_fine=$sum_after_recovery_loss=$sum_per_kg_loss=$sum_before_recovery_loss=$sum_recoverd_loss_fine=$sum_out_weight=$sum_fine=$sum_unrecoverable_loss=$sum_balance=$sum_out_weight=0;
      foreach ($loss_categories as $index => $loss_category) {
-      $sum_overall_loss_fine+=four_decimal($loss_category['overall_loss_fine']);
+      $sum_loss_fine+=four_decimal($loss_category['loss_fine']);
       $sum_out_weight+=four_decimal($loss_category['out_weight']);
+      $sum_per_kg_loss+=four_decimal($loss_category['per_kg_loss']);
+      $sum_before_recovery_loss+=four_decimal($loss_category['before_recovery_loss']);
       $sum_recoverd_loss_fine+=four_decimal($loss_category['recoverd_loss_fine']);
-      $sum_all_loss_before_recovery+=four_decimal($loss_category['all_loss_before_recovery']);
-      $sum_all_loss_after_recovery+=four_decimal($loss_category['all_loss_after_recovery']);
-      $sum_unrecovery_loss+=four_decimal($loss_category['unrecovery_loss']);
-      $sum_per_kg_loss+=!empty($loss_category['out_weight'])?four_decimal($loss_category['overall_loss_fine']/$loss_category['out_weight']*1000):0;
+      $sum_after_recovery_loss+=four_decimal($loss_category['after_recovery_loss']);
+      $sum_unrecoverable_loss+=four_decimal($loss_category['unrecoverable_loss']);
+      $sum_balance+=four_decimal($loss_category['balance']);
       ?>
       <tr>
         <td class=""><a href="<?=base_url()?>reports/loss_report_details?category=<?=$index ?>&factory_name=<?=$factory_name ?>"><?=$index?></a></td>
-        <td class="text-right"><?=four_decimal($loss_category['overall_loss_fine'])?></td>
-        <td class="text-right"><?=four_decimal($loss_category['out_weight'])?></td>
-        <td class="text-right"><?=!empty($loss_category['out_weight'])?four_decimal($loss_category['overall_loss_fine']/$loss_category['out_weight']*1000):0?></td>
+        <td class="text-right"><?=four_decimal($loss_category['loss_fine'])?></td>
+        <td class="text-right"><?=!empty($loss_category['out_weight'])?four_decimal($loss_category['out_weight']):0;?></td>
+        <td class="text-right"><?=!empty($loss_category['per_kg_loss'])?four_decimal($loss_category['per_kg_loss']):0;?></td>
         
-        <td class="text-right"><?=four_decimal($loss_category['recoverd_loss_fine'])?></td>
-        <td class="text-right"><?=four_decimal($loss_category['all_loss_before_recovery'])?></td>
-        <td class="text-right"><?=four_decimal($loss_category['all_loss_after_recovery'])?></td>
-        <td class="text-right"><?=four_decimal($loss_category['unrecovery_loss']) ?></td>
+        <td class="text-right"><?=!empty($loss_category['before_recovery_loss'])?four_decimal($loss_category['before_recovery_loss']):0;?></td>
+        <td class="text-right"><?=!empty($loss_category['recoverd_loss_fine'])?four_decimal($loss_category['recoverd_loss_fine']):0;?></td>
+        <td class="text-right"><?=!empty($loss_category['after_recovery_loss'])?four_decimal($loss_category['after_recovery_loss']):0;?></td>
+        <td class="text-right"><?=!empty($loss_category['unrecoverable_loss'])?four_decimal($loss_category['unrecoverable_loss']):0; ?></td>
+        <td class="text-right"><?=!empty($loss_category['balance'])?four_decimal($loss_category['balance']):0; ?></td>
        
       </tr>
     <?php }?>
       <tr class="bg_gray bold">
     <td>Total</td>
-    <td class="text-right"><?=$sum_overall_loss_fine?></td>
+    <td class="text-right"><?=$sum_loss_fine?></td>
     <td class="text-right"><?=$sum_out_weight?></td>
     <td class="text-right"><?=$sum_per_kg_loss?></td>
+    <td class="text-right"><?=$sum_before_recovery_loss?></td>
     <td class="text-right"><?=$sum_recoverd_loss_fine?></td>
-    <td class="text-right"><?=$sum_all_loss_before_recovery?></td>
-    <td class="text-right"><?=$sum_all_loss_after_recovery?></td>
-    <td class="text-right"><?=$sum_unrecovery_loss?></td>
+    <td class="text-right"><?=$sum_after_recovery_loss?></td>
+    <td class="text-right"><?=$sum_unrecoverable_loss?></td>
+    <td class="text-right"><?=$sum_balance?></td>
   </tr>
     </tbody>
   </table>
