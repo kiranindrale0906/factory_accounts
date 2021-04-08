38bab56456b6074cf6879150b22290c85e81ed23
kiran <kiran.indrale@ascratech.com>
2021-04-08 14:04:56 +0530

    chaneg

--- application/config/config.php
+++ application/config/config.php
@@ -545,3 +545,8 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:    array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+define('API_ARG_JAN2021_PATH', 'https://staging-argold.ascratech.com/');
+define('API_ARF_JAN2021_PATH', 'https://staging-arf.ascratech.com/');
+define('API_ARC_JAN2021_PATH', 'https://staging-arc.ascratech.com/');
+define('HOST', '');
+
