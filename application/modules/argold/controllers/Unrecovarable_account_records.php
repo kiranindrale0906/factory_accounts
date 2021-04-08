2b2e8e45c56c50cf68e7678a7c1a7ba988ae9b41
kiran <kiran.indrale@ascratech.com>
2021-04-08 11:24:06 +0530

    change

--- application/modules/argold/controllers/Unrecovarable_account_records.php
+++ application/modules/argold/controllers/Unrecovarable_account_records.php
@@ -6,7 +6,7 @@ class Unrecovarable_account_records extends BaseController {
   public function __construct() {
     parent::__construct();
     $this->redirect_after_save = 'view';
-    $this->load->model(array('ac_vouchers/voucher_model','transactions/metal_issue_voucher_model','masters/account_model'));
+    $this->load->model(array('ac_vouchers/voucher_model','transactions/metal_issue_voucher_model','transactions/metal_receipt_voucher_model','masters/account_model'));
   }
   public function store() {
     if(isset($_GET['from']) && $_GET['from']=='view') {
@@ -18,6 +18,8 @@ class Unrecovarable_account_records extends BaseController {
         'company_id'=>1,
         'parent_id'=>$_GET['parent_id'],
         'voucher_date'=>date('Y-m-d'),
+        'factory_purity'=>100,
+        'factory_fine'=>$_GET['credit_weight'],
         'credit_weight'=>$_GET['credit_weight']);
       $issue_obj = new metal_issue_voucher_model($process);
       $issue_obj->before_validate();
@@ -27,11 +29,13 @@ class Unrecovarable_account_records extends BaseController {
         'account_name'=>$_GET['factory'],
         'narration'=>$_GET['narration'],
         'receipt_type'=>$_GET['account_name'],
+        'debit_weight'=>$_GET['credit_weight'],
         'purity'=>100,
+        'factory_purity'=>100,
+        'factory_fine'=>$_GET['credit_weight'],
         'company_id'=>1,
         'parent_id'=>$_GET['parent_id'],
-        'voucher_date'=>date('Y-m-d'),
-        'debit_weight'=>$_GET['credit_weight']);
+        'voucher_date'=>date('Y-m-d'));
       $receipt_obj = new metal_receipt_voucher_model($process);
       $receipt_obj->before_validate();
       $receipt_obj->save(true);
