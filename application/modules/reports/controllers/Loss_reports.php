2bc1b4c5b21ab8a5c4000546b9ca979906914453
kiran <kiran.indrale@ascratech.com>
2021-04-13 16:07:02 +0530

    chaneg

--- application/modules/reports/controllers/Loss_reports.php
+++ application/modules/reports/controllers/Loss_reports.php
@@ -74,7 +74,6 @@ class Loss_reports extends BaseController {
         $ghiss_melting_loss=$this->voucher_model->get('description,site_name,credit_weight as in_weight,purity as in_lot_purity,argold_id as parent_id,0 as out_weight', array('account_name'=>'Loss Account','site_name'=>'AR Gold st','receipt_type'=>'Ghiss Melting Loss'),array());
         foreach ($ghiss_melting_loss as $ghiss_melting_loss_index => $ghiss_melting_loss_value) {
           $data['issue_department_id']=$ghiss_melting_loss_value['parent_id'];
-          pd($data);
           $url=API_ARG_JAN2021_PATH."issue_and_receipts/loss_report_for_accounts/index";
           $ghiss_details=json_decode(curl_post_request($url,$data),true);
           $out_weight=!empty($ghiss_details)?$ghiss_details['data']['ghiss_melting_out_weights']:0;
