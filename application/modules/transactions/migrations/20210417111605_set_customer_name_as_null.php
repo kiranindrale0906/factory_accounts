bf9c2b82262768d259457018e209457ec73fe464
kiran <kiran.indrale@ascratech.com>
2021-04-17 11:18:08 +0530

    chaneg

---
+++ application/modules/transactions/migrations/20210417111605_set_customer_name_as_null.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_set_customer_name_as_null extends CI_Model {
+
+  public function up()
+  {
+    // $this->db->query("");
+  }
+
+
+}
+
+?>
