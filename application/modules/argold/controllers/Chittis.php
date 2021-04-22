c7e92a580d12dd3bcd272da0b140f82ac084b9a5
kiran <kiran.indrale@ascratech.com>
2021-04-22 10:55:26 +0530

    change

--- application/modules/argold/controllers/Chittis.php
+++ application/modules/argold/controllers/Chittis.php
@@ -73,12 +73,11 @@ class Chittis extends BaseController {
       $this->data['chittis_details'] = @$_POST['chittis_details'];
     }
 
-    $this->data['site_names'] = array(array('id' => 'AR Gold Jan 2021', 'name' => 'AR Gold Jan 2021'),
+    $this->data['site_names'] = array(
+                                      array('id' => 'AR Gold Jan 2021', 'name' => 'AR Gold Jan 2021'),
                                       array('id' => 'ARF Jan 2021', 'name' => 'ARF Jan 2021'),
                                       array('id' => 'ARC Jan 2021', 'name' => 'ARC Jan 2021'),
-                                      array('id' => 'AR Gold Nov 2020', 'name' => 'AR Gold Nov 2020'),
-                                      array('id' => 'ARF Nov 2020', 'name' => 'ARF Nov 2020'),
-                                      array('id' => 'ARC Nov 2020', 'name' => 'ARC Nov 2020'));
+                                     );
   }
 
   public function store() {
