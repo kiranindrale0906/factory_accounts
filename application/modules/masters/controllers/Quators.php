3ed66ec2f0a1e0566c457e5cb2b87dc9d940c9d4
kiran <kiran.indrale@ascratech.com>
2021-04-27 20:29:25 +0530

    chaneg

--- application/modules/masters/controllers/Quators.php
+++ application/modules/masters/controllers/Quators.php
@@ -9,6 +9,7 @@ class Quators extends BaseController {
 
     }
     public function index() {
+    	pd($_POST);
 	    if(!empty($_POST['get_quators'])) {
 	      $data=$this->model->get('name,name as id', array('name!='=>''));
 	      echo json_encode(array('data'=>$data,'status'=>'success')); die;
