851ec7c611d40e6ebcd59db1fe5ec4b73dc088e7
Atul Agrawal <atul@ascratech.com>
2021-05-28 11:16:53 +0530

    changes

--- application/modules/reports/views/trial_balances/gst_fields.php
+++ application/modules/reports/views/trial_balances/gst_fields.php
@@ -1,25 +1,25 @@
-<?php if ($gst['taxable_amount'] > 0) { ?>
+<?php if ($gst['taxable_amount'] != 0) { ?>
   <tr>
     <td><?= $gst_voucher ?> TAXABLE</td>
     <td class="text-right"><?= four_decimal($gst['taxable_amount'], '-') ?></td>
   </tr>
 <?php } ?>
 
-<?php if ($gst['cgst_amount'] > 0) { ?>
+<?php if ($gst['cgst_amount'] != 0) { ?>
   <tr>
     <td><?= $gst_voucher ?> CGST</td>
     <td class="text-right"><?= four_decimal($gst['cgst_amount'], '-') ?></td>
   </tr>
 <?php } ?>
 
-<?php if ($gst['sgst_amount'] > 0) { ?>
+<?php if ($gst['sgst_amount'] != 0) { ?>
   <tr>
     <td><?= $gst_voucher ?> SGST</td>
     <td class="text-right"><?= four_decimal($gst['sgst_amount'], '-') ?></td>
   </tr>
 <?php } ?>
 
-<?php if ($gst['tcs_amount'] > 0) { ?>
+<?php if ($gst['tcs_amount'] != 0) { ?>
   <tr>
     <td><?= $gst_voucher ?> TCS</td>
     <td class="text-right"><?= four_decimal($gst['tcs_amount'], '-') ?></td>
