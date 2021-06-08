6ad4808066367e7049ad587606c8578aee05e0c6
kiran <kiran1234>
2021-06-08 11:45:48 +0530

    change

--- application/modules/argold/views/chitti_details/formlist.php
+++ application/modules/argold/views/chitti_details/formlist.php
@@ -10,8 +10,8 @@
       <th class="text-right">Weight</th>
       <th class="text-right">Factory Purity</th>
       <th class="text-right">Wastage</th>
-      <th class="text-right">USD Wastage</th>
-      <th class="text-right">INR Wastage</th>
+      <!-- <th class="text-right">USD Wastage</th>
+      <th class="text-right">INR Wastage</th> -->
       <th class="text-right">Issue Fine</th>
     </tr>
   </thead>
@@ -33,8 +33,8 @@
       <th class="text-right"><?= four_decimal($weight) ?></th>
       <th></th>
       <th></th>
-      <th></th>
-      <th></th>
+      <!-- <th></th>
+      <th></th> -->
       <th class="text-right"><?= four_decimal($issue_fine) ?></th>
     </tr>
   </tbody>
