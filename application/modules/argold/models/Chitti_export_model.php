66a5bd5dfef57db2805764955a872ae7163efce4
kiran <kiran1234>
2021-06-08 15:02:59 +0530

    chaneg

--- application/modules/argold/models/Chitti_export_model.php
+++ application/modules/argold/models/Chitti_export_model.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
- require_once APPPATH . "modules/".CLIENT_NAME."/models/chitti_model.php";  
+ require_once APPPATH . "modules/argold/models/chitti_model.php";  
   class Chitti_export_model extends chitti_model {
   	public $router_class = "chitti_exports";
     function __construct($data=array()) {
