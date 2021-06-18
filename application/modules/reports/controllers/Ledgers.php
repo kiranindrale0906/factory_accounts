6e82dacf16a989d2d2bdc97954b19fea83fab894
kiran <kiran1234>
2021-06-18 11:41:15 +0530

    change

--- application/modules/reports/controllers/Ledgers.php
+++ application/modules/reports/controllers/Ledgers.php
@@ -138,7 +138,7 @@ class Ledgers extends BaseController {
       $where_issue   = array_merge($where, array('(credit_weight != 0 or credit_amount != 0)' => NULL),$account_issue_where);
       $where_receipt = array_merge($where, array('(debit_weight != 0 or debit_amount != 0)'   => NULL),$account_receipt_where);
       $issues   = $this->ledger_model->get($receipt_issue_select, $where_issue,   array(), array('order_by'=>'chitti_id, voucher_type, str_voucher_date asc', 'group_by' => $this->data['group']));
-      lq();
+      
       $receipts = $this->ledger_model->get($receipt_issue_select, $where_receipt, array(), array('order_by'=>'parent_id, voucher_type, str_voucher_date asc', 'group_by' => $this->data['group']));
       
 
