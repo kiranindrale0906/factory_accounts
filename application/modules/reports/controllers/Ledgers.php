1053a6c997fc9775a80276e41c54356cf24a389c
kiran <kiran1234>
2021-06-24 18:20:18 +0530

    chNGE

--- application/modules/reports/controllers/Ledgers.php
+++ application/modules/reports/controllers/Ledgers.php
@@ -140,7 +140,6 @@ class Ledgers extends BaseController {
       $where_receipt = array_merge($where, array('(debit_weight != 0 or debit_amount != 0)'   => NULL),$account_receipt_where);
       $issues   = $this->ledger_model->get($receipt_issue_select, $where_issue,   array(), array('order_by'=>'chitti_id, voucher_type, str_voucher_date asc', 'group_by' => $this->data['group']));
       $receipts = $this->ledger_model->get($receipt_issue_select, $where_receipt, array(), array('order_by'=>'parent_id, voucher_type, str_voucher_date asc', 'group_by' => $this->data['group']));
-      lq();
       
 
     $issue_voucher_dates = array_column($issues, 'voucher_date');
