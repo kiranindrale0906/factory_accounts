57738759c58f79dda2a3fb51a67db0f9479c49d9
kiran <kiran1234>
2021-06-24 15:14:33 +0530

    chaneg

--- application/modules/reports/controllers/Loss_summaries.php
+++ application/modules/reports/controllers/Loss_summaries.php
@@ -87,6 +87,7 @@ class Loss_summaries extends BaseController {
           $url=$path."issue_and_receipts/loss_report_for_accounts/index";
           $records=json_decode(curl_post_request($url,$data),true);
       }
+      pd($records);
       foreach ($category_names as $index => $category_name) {
         $total_unrecovery_loss=0;
         foreach ($records as $key => $value) {
