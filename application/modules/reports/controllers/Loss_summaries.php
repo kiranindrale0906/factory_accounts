ad8c5316e4eb9f13cc9d889daaf8d8d15620ab43
kiran <kiran1234>
2021-06-24 18:27:11 +0530

    chNGE

--- application/modules/reports/controllers/Loss_summaries.php
+++ application/modules/reports/controllers/Loss_summaries.php
@@ -18,10 +18,9 @@ class Loss_summaries extends BaseController {
     $arc_loss_records=$this->loss_details('ARC',$this->data['quator']['from_date'],$this->data['quator']['to_date']);
     $accounts_balance_select = '(sum(debit_weight) - sum(credit_weight)) as balance, (sum(debit_weight*purity/100) - sum(credit_weight*purity/100)) as balance_fine';
 
-    $argold_vodator = $this->ledger_model->find($accounts_balance_select, array('site_name' => 'AR Gold Jan 2021','purity != factory_purity'=>NULL,'account_name != '=>"VADOTAR"));
+    $argold_vodator = $this->ledger_model->find($accounts_balance_select, array('site_name' => 'AR Gold Jan 2021','purity != factory_purity'=>NULL,'account_name != '=>"VADOTAR"),array(),array('group_by'=>'voucher_date'));
 
-    $arf_vodator = $this->ledger_model->find($accounts_balance_select, array("`site_name` = 'ARF Jan 2021' AND `purity` != `factory_purity` AND `account_name` != 'VADOTAR' AND (`debit_weight` !=0 or `debit_amount` !=0 or `credit_weight` !=0 or `credit_amount` !=0)"=>NULL));
-    lq();
+    $arf_vodator = $this->ledger_model->find($accounts_balance_select, array("`site_name` = 'ARF Jan 2021' AND `purity` != `factory_purity` AND `account_name` != 'VADOTAR' AND (`debit_weight` !=0 or `debit_amount` !=0 or `credit_weight` !=0 or `credit_amount` !=0)"=>NULL),array(),array('group_by'=>'voucher_date'));
     
 
     $arc_vodator = $this->ledger_model->find($accounts_balance_select, array('site_name' => 'ARC Jan 2021','purity != factory_purity'=>NULL,'account_name != '=>"VADOTAR"));
