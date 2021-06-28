567cf0c7c055fe0953c117105881f0e4b3c7808b
Atul Agrawal <atul@ascratech.com>
2021-06-29 03:05:21 +0530

    changes

--- application/modules/reports/controllers/Loss_summaries.php
+++ application/modules/reports/controllers/Loss_summaries.php
@@ -13,9 +13,9 @@ class Loss_summaries extends BaseController {
     $this->data['quators'] = $this->quator_model->get('name');
     $this->data['quator'] = $this->quator_model->find('*',array('name'=>$this->data['quator_name']));
     if(!empty($this->data['quator'])){
-    $arg_loss_records=$this->loss_details('ARG',$this->data['quator']['from_date'],$this->data['quator']['to_date']);
-    $arf_loss_records=$this->loss_details('ARF',$this->data['quator']['from_date'],$this->data['quator']['to_date']);
-    $arc_loss_records=$this->loss_details('ARC',$this->data['quator']['from_date'],$this->data['quator']['to_date']);
+    $arg_loss_records=$this->loss_details('ARG', $this->data['quator']);
+    $arf_loss_records=$this->loss_details('ARF', $this->data['quator']);
+    $arc_loss_records=$this->loss_details('ARC', $this->data['quator']);
     $accounts_balance_select = 'sum(debit_weight)-sum(credit_weight) as balance,date_format(voucher_date,"%Y-%m") as voucher_date';
 
     $argold_production = $this->ledger_model->get($accounts_balance_select, array('site_name = "AR Gold Jan 2021"' => NULL,'purity != factory_purity'=>NULL,'account_name != "VADOTAR"'=> NULL,'date(voucher_date)>='=>$this->data['quator']['from_date'],'date(voucher_date)<='=>$this->data['quator']['to_date']),array(),array('group_by'=>'date_format(voucher_date,"%Y-%m")'));
@@ -105,7 +105,9 @@ class Loss_summaries extends BaseController {
     $this->load->render($this->router->class."/index",$this->data);
   }
 
-  private function loss_details($site_name,$from_date,$to_date){
+  private function loss_details($site_name, $quator){
+    $from_date = $this->data['quator']['from_date'];
+    $to_date = $this->data['quator']['to_date'];
     $categories= $this->voucher_model->get('description', array('account_name'=>'Loss Account','date(created_at)>='=>'2021-03-13'),array(),array('group_by'=>'description'));
       $category_names=array_column($categories,'description');
       $data['department_names']=$category_names;
@@ -123,8 +125,7 @@ class Loss_summaries extends BaseController {
       }
       if(!empty($data['department_names'])){
           $url=$path."issue_and_receipts/loss_report_for_accounts/index";
-          $factory_records=json_decode(curl_post_request($url,$data),true);
-          pd($factory_records);
+          $factory_records=json_decode(curl_post_request($url,array_merge($data, array('quator' => $quator['name'])),true);
           $records = !empty($factory_records) ? $factory_records['data']['loss_details']['loss_detail'] : $factory_records['data']['loss_details']['loss_detail']=array();
           $ghiss_melting_loss=$this->voucher_model->get('description,site_name,credit_weight as in_weight,purity as in_lot_purity,argold_id as parent_id,0 as out_weight', array('account_name'=>'Loss Account',
                                               'site_name'=>$factory_name,
