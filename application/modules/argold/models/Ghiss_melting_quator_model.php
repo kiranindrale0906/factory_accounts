97cd16e125e7ffae4a57780263e99b9c2160b84a
kiran <kiran1234>
2021-06-29 17:01:05 +0530

    chaneg

--- application/modules/argold/models/Ghiss_melting_quator_model.php
+++ application/modules/argold/models/Ghiss_melting_quator_model.php
@@ -10,7 +10,7 @@ class Ghiss_melting_quator_model extends BaseModel {
   }
 
   public function validation_rules($klass='') {
-    $rules[] = array('field' => 'ghiss_melting_quator[id]', 
+    $rules[] = array('field' => 'ghiss_melting_quators[id]', 
                      'label' => 'Id',
                      'rules' => 'trim|required');
     return $rules;
