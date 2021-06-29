ab5635d6b99ec4661f990c50bcd424400a842742
kiran <kiran1234>
2021-06-29 16:41:52 +0530

    chaneg

---
+++ application/modules/transactions/migrations/20210629163930_add_quator_in_ac_accounts.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_add_quator_in_ac_accounts extends CI_Model {
+
+  public function up()
+  {
+    $this->db->query("ALTER TABLE `ac_vouchers` ADD `quator` VARCHAR(225) NOT NULL;");
+  }
+
+
+}
+
+?>
