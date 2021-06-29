e7e63cbe51bb7d140cae47365ecdb42fd6c1cc3e
kiran <kiran1234>
2021-06-29 17:23:57 +0530

    change

--- application/modules/argold/controllers/Ghiss_melting_quators.php
+++ application/modules/argold/controllers/Ghiss_melting_quators.php
@@ -5,7 +5,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Ghiss_melting_quators extends BaseController {
   public function __construct() {
     parent::__construct();
-    $this->redirect_after_save = 'view';
     $this->load->model(array('masters/quator_model'));
   }
   public function _get_form_data() {
