4c3aeaa4227f869fe1625e48d7f1806cb1534ffa
Atul Agrawal <atul@ascratech.com>
2021-06-30 19:03:55 +0530

    changes;

--- application/modules/reports/controllers/Trial_balances.php
+++ application/modules/reports/controllers/Trial_balances.php
@@ -323,7 +323,7 @@ class Trial_balances extends Ledgers {
   private function calculate_profit_loss_of_export_sales_accounts($sale_type){               
     $where = array();               
     
-    $where['ounce_rate != 0 || usd_rate != 0'] = 0;
+    $where['ounce_rate != 0 or usd_rate != 0'] = 0;
     if ($sale_type == 'Labour')
       $select = 'sum(labour_usd_amount * usd_rate) + sum(freight_usd_amount * usd_rate) as taxable_amount, 0 as cgst_amount, 0 as sgst_amount, 0 as tcs_amount, 0 as factory_fine'; 
     else
