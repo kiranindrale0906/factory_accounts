617c151f9af6f8c5872a14f3f6b89831d28d6e81
kiran <kiran1234>
2021-07-16 14:20:33 +0530

    chaneg

--- application/modules/masters/controllers/Accounts.php
+++ application/modules/masters/controllers/Accounts.php
@@ -11,9 +11,13 @@ class Accounts extends BaseController {
   }
 
   public function index() {
-    if(!empty($_POST['account_name'])) {
+    if(!empty($_POST['account_name']) ) {
       $data=$this->model->get('name', array('name like "%'.$_POST['account_name'].'%"' => NULL,
                                             'name not like "%Nov 2020%"' => NULL));
+      echo json_encode(array('data'=>$data,'status'=>'success')); die;
+    }
+    if(!empty($_POST['sub_group_name'])) {
+      $data=$this->model->get('name', array('sub_group_name' => $_POST['sub_group_name']));
       echo json_encode(array('data'=>$data,'status'=>'success')); die;
     }
 
