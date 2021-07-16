5a41e3c865da1c8c235f16f5b144bda9a7d0fdef
kiran <kiran1234>
2021-07-16 19:23:27 +0530

    change

--- application/modules/argold/controllers/Chittis.php
+++ application/modules/argold/controllers/Chittis.php
@@ -169,8 +169,7 @@ class Chittis extends BaseController {
     $this->data['site_names'] = array(
                                       array('id' => 'AR Gold Jan 2021', 'name' => 'AR Gold Jan 2021'),
                                       array('id' => 'ARF Jan 2021', 'name' => 'ARF Jan 2021'),
-                                      array('id' => 'ARC Jan 2021', 'name' => 'ARC Jan 2021'),
-                                      array('id' => 'AR Gold staging', 'name' => 'AR Gold staging'),
+                                      array('id' => 'ARC Jan 2021', 'name' => 'ARC Jan 2021')),
                                      );
   }
 
