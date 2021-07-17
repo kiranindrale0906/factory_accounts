7759cb9d470ede30294fdc8214ca72798382d9c1
kiran <kiran1234>
2021-07-17 13:04:24 +0530

    chaneg

--- application/modules/argold/controllers/Chittis.php
+++ application/modules/argold/controllers/Chittis.php
@@ -153,12 +153,12 @@ class Chittis extends BaseController {
       }
     }
 
+  }
     $this->data['site_names'] = array(
                                       array('id' => 'AR Gold Jan 2021', 'name' => 'AR Gold Jan 2021'),
                                       array('id' => 'ARF Jan 2021', 'name' => 'ARF Jan 2021'),
                                       array('id' => 'ARC Jan 2021', 'name' => 'ARC Jan 2021')
                                      );
-  }
 }
 
   public function store() {
