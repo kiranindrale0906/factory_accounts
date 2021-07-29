9aabd115285d7a7a762918cf908ef25ba1dc8022
kiran <kiran1234>
2021-07-29 11:31:37 +0530

    chaneg

--- application/modules/argold/controllers/Packing_slips.php
+++ application/modules/argold/controllers/Packing_slips.php
@@ -41,7 +41,7 @@ class Packing_slips extends BaseController {
     
     if (!empty($_GET['account_name']))
       $this->data['record']['account_name'] = $_GET['account_name'];
-    $where=array('voucher_type' => 'metal issue voucher','packing_slip_balance=' => 0,);
+    $where=array('voucher_type' => 'metal issue voucher','packing_slip_balance!=' => 0,);
     
     if(!empty($this->data['record']['account_name'])) { 
       $where['account_name']=$this->data['record']['account_name'];
