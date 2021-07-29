c8f25740c48ab09c3935d166b2a50373e0656646
kiran <kiran1234>
2021-07-29 12:34:26 +0530

    chaneg

--- application/modules/argold/controllers/Packing_slips.php
+++ application/modules/argold/controllers/Packing_slips.php
@@ -27,8 +27,6 @@ class Packing_slips extends BaseController {
   }
 
   public function _get_form_data() {
-
-    $this->db->query("update ac_vouchers set packing_slip_balance=credit_weight;");
     $this->data['account_name']=$this->account_model->get('distinct(name) as name,name as id',array('group_code'=>'Export'));
     $this->data['purity'] = $this->voucher_model->get('purity as name, purity as id', 
                                                        array('where'=>array(
