6f9a43f04cbb8781296137084f1f15f4cf788b23
kiran <kiran1234>
2021-07-30 16:21:28 +0530

    change

--- application/modules/argold/views/packing_slip_details/taglist.php
+++ application/modules/argold/views/packing_slip_details/taglist.php
@@ -3,8 +3,8 @@
     //foreach ($packet_nos as $packet_index => $packet_no) { 
     ?>
       <!-- <h6>Packet No :<?=$packet_no ?> </h6>     -->
-      <div class="" style="font-weight:500">
-      <table class="table">
+      <div class="" style="font-weight:300">
+      <table class="">
           <tr>
             <?php foreach ($metal_voucher_details as $index => $metal_voucher_detail) {
                    ?>
