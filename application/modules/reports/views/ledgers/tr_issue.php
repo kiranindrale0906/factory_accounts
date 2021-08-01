4d4f71a809e8a2285f832ad566c1739fa2d6bf2a
Atul Agrawal <atul@ascratech.com>
2021-08-01 23:39:55 +0530

    changes

--- application/modules/reports/views/ledgers/tr_issue.php
+++ application/modules/reports/views/ledgers/tr_issue.php
@@ -29,7 +29,7 @@
     <td class="text-right"><?= four_decimal($record['factory_fine']-$record['fine'], '-'); ?></td>
     <td class="text-right"><?= four_decimal(($record['factory_fine']-$record['fine']) / $record['credit_weight'] * 100, '-'); ?></td>
   <?php endif; ?>
-  <?php elseif ($report_type == 'Account Ledger' || $report_type == 'Vadotar Report'): ?>
+  <?php if ($report_type == 'Account Ledger' || $report_type == 'Vadotar Report'): ?>
     <td class="text-right"><a class=""  href='<?= base_url() ?>argold/voucher_details/view/<?=$record['chitti_no']?>'><?= four_decimal($record['credit_amount'], '-') ?></a></td>
     <td class="text-right"><?= four_decimal($record['usd_credit_amount'], '-') ?></td>
   <?php endif; ?>
