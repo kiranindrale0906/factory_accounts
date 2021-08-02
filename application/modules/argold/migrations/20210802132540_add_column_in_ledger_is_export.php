eab51e7f0fe6ba65f55f98bc0e22c016aed28f58
kiran <kiran1234>
2021-08-02 13:27:05 +0530

    change

---
+++ application/modules/argold/migrations/20210802132540_add_column_in_ledger_is_export.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_add_column_in_ledger_is_export extends CI_Model {
+
+  public function up()
+  {
+    $this->db->query("ALTER TABLE `ac_ledger` ADD `is_export` int(11) NULL DEFAULT 0");
+  }
+
+
+}
+
+?>
