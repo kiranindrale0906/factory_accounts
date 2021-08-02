56c11dd6b6fdaf0b2b584812aeed278991b3a7e4
Atul Agrawal <atul@ascratech.com>
2021-08-02 17:11:46 +0530

    changes

--- application/modules/reports/views/ledgers/search.php
+++ application/modules/reports/views/ledgers/search.php
@@ -47,30 +47,30 @@
     <div class="form-group container"> 
       <h5> Select Period: 
         <a class="ml-5 <?= ($period=='date') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=date&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=<?= $group ?>'>Date</a>
+           href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=date&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=<?= $group ?>&domestic_export=<?= $domestic_export ?>'>Date</a>
         <a class="ml-5 <?= ($period=='week') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=week&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=<?= $group ?>'>Week</a>
+           href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=week&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=<?= $group ?>&domestic_export=<?= $domestic_export ?>'>Week</a>
         <a class="ml-5 <?= ($period=='month') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=month&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=<?= $group ?>'>Month</a>
+           href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=month&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=<?= $group ?>&domestic_export=<?= $domestic_export ?>'>Month</a>
         <a class="ml-5 <?= ($period=='year') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=year&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=<?= $group ?>'>Year</a>
+           href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=year&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=<?= $group ?>&domestic_export=<?= $domestic_export ?>'>Year</a>
       </h5>
     </div>
     <!-- <div class="form-group container"> 
       <h5> Details: 
         <a class="ml-5 <?= ($detail=='yes') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=yes&group=<?= $group ?>'>Show Details</a>
+           href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=yes&group=<?= $group ?>&domestic_export=<?= $domestic_export ?>'>Show Details</a>
         <a class="ml-5 <?= ($detail=='no') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=no&group=<?= $group ?>'>Hide Details</a>
+           href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=no&group=<?= $group ?>&domestic_export=<?= $domestic_export ?>'>Hide Details</a>
       </h5>
     </div> -->
     <?php if ($report_type != 'Rojmel Report' && $report_type != 'Account Receipt Report' && $report_type != 'Metal Receipt Type Report') { ?>
       <div class="form-group container"> 
         <h5> Group By: 
           <a class="ml-5 <?= ($group=='') ? 'bold black underline' : '' ?>" 
-             href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group='>None</a>
+             href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=&domestic_export=<?= $domestic_export ?>'>None</a>
           <a class="ml-5 <?= ($group=='voucher_date') ? 'bold black underline' : '' ?>" 
-             href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=voucher_date'>Date</a>
+             href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=voucher_date&domestic_export=<?= $domestic_export ?>'>Date</a>
         </h5>
       </div>
     <?php } ?>
@@ -84,12 +84,27 @@
 
         <?php } if($_SESSION['vodator_report']==1){ ?>
           <a class="ml-5 <?= ($report_type=='Vadotar Report') ? 'bold black underline' : '' ?>" 
-             href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=<?= $period ?>&report_type=Vadotar Report&detail=<?= $detail ?>&group=<?= $group ?>'>Vadotar Report</a>
+             href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=<?= $period ?>&report_type=Vadotar Report&detail=<?= $detail ?>&group=<?= $group ?>&domestic_export=<?= $domestic_export ?>'>Vadotar Report</a>
         <?php }?>
         <?php if($_SESSION['production_report']==1){ ?>
           <a class="ml-5 <?= ($report_type=='Production Report') ? 'bold black underline' : '' ?>" 
-             href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=<?= $period ?>&report_type=Production Report&detail=<?= $detail ?>&group=<?= $group ?>'>Production Report</a>
+             href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=<?= $period ?>&report_type=Production Report&detail=<?= $detail ?>&group=<?= $group ?>&domestic_export=<?= $domestic_export ?>'>Production Report</a>
         <?php }?>
+        </h5>
+      </div>
+
+      <div class="form-group container"> 
+        <h5> 
+        <?php if($_SESSION['vodator_report']==1 || $_SESSION['production_report']==1){ ?>
+        Domestic / Export: 
+          <a class="ml-5 <?= ($domestic_export=='All') ? 'bold black underline' : '' ?>" 
+             href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=<?= $group ?>&domestic_export=All'>All</a>
+          <a class="ml-5 <?= ($domestic_export=='Domestic') ? 'bold black underline' : '' ?>" 
+             href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=<?= $group ?>&domestic_export=Domestic'>Domestic</a>
+          <a class="ml-5 <?= ($domestic_export=='Export') ? 'bold black underline' : '' ?>" 
+             href='<?= base_url().$url ?>?account_id=<?= $account_id ?>&site_name=<?= $site_name?>&period=<?= $period ?>&report_type=<?= $report_type ?>&detail=<?= $detail ?>&group=<?= $group ?>&domestic_export=Export'>Export</a>
+        <?php }?>
+        
         </h5>
       </div>
     <?php } }?>
