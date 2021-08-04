f34625f37a69e3e04e83cd7cce85aa1f8ef5db78
kiran <kiran1234>
2021-08-04 18:11:26 +0530

    chnage

--- application/modules/argold/models/Client_metal_receipt_voucher_model.php
+++ application/modules/argold/models/Client_metal_receipt_voucher_model.php
@@ -519,8 +519,8 @@ class Client_metal_receipt_voucher_model extends Core_metal_receipt_voucher_mode
           $api_url = API_ARC_NOV2020_PATH.$api_url;
     elseif ($attributes['account_name'] == 'ARF Software Staging')
           $api_url = API_ARF_NOV2020_PATH.$api_url;
-        pd($api_url);
     $result = curl_post_request($api_url, $send_data);
+        pd($result);
   }
 
   public function create_vodator_records($records, $receipt_type, $site_name, $hostversion) {
