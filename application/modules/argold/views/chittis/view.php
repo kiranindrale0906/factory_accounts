aefba8f9afb8fdd37e64b174032041e134ff8ba7
kiran <kiran1234>
2021-08-24 15:39:15 +0530

    chnge

--- application/modules/argold/views/chittis/view.php
+++ application/modules/argold/views/chittis/view.php
@@ -30,7 +30,9 @@
     </tr><tr>
       <td>Sale Type</td><td class="text-right"><h6><?= $record['sale_type'] ?></h6></td>
     </tr>
-    <?php if (!empty($record['no_of_packets']) && $record['no_of_packets'] > 0) { ?>
+    <?php 
+    pd($record);
+    if (!empty($record['no_of_packets']) && $record['no_of_packets'] > 0) { ?>
       <tr>
         <td>No of Packets</td><td class="text-right"><h6><?=round($record['no_of_packets'])?></h6></td>
       </tr>
