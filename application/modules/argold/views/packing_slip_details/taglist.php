41e6617c27dbf6b5989616f0b5d5c60e6e3116dc
kiran <kiran1234>
2021-09-01 16:04:19 +0530

    chaneg

--- application/modules/argold/views/packing_slip_details/taglist.php
+++ application/modules/argold/views/packing_slip_details/taglist.php
@@ -5,15 +5,26 @@
       <!-- <h6>Packet No :<?=$packet_no ?> </h6>     -->
       <div class="" style="font-weight:500">
       <table class="" width="100%"  style="font-size: 11px">
-          <tr>
             <?php foreach ($metal_voucher_details as $index => $metal_voucher_detail) {
+
                    ?>
+          <tr>
             <td class="" style="padding: 2px;">Tag No.</td>
             <td style="padding: 2px" class="text-center"><?= ($metal_voucher_detail['sr_no']); ?></td>
             
+          </tr>
+          <tr>
+            <td style="padding: 2px" class="">Gross Wt</td>
+            <td style="padding: 2px" class="text-center"><?= four_decimal($metal_voucher_detail['gross_weight']); ?></td>
+            
+            
+          </tr>
+          <tr>
+            <td style="padding: 2px" class="">Net Wt</td>
+            <td style="padding: 2px" class="text-center"><?= four_decimal($metal_voucher_detail['net_weight']) ?></td>
+          </tr>
            <?php 
             }?>
-          </tr>
           <tr>
             <?php foreach ($metal_voucher_details as $index => $metal_voucher_detail) {
                    ?>
@@ -23,53 +34,7 @@
            <?php 
             }?>
           </tr>
-          <tr>
-            <?php
-            foreach ($metal_voucher_details as $index => $metal_voucher_detail) {
-               ?> 
-            
-            <td style="padding: 2px" class="">Net Wt</td>
-            <td style="padding: 2px" class="text-center"><?= four_decimal($metal_voucher_detail['net_weight']) ?></td>
-             <?php 
-            }?>
-            </tr><tr><?php
-            foreach ($metal_voucher_details as $index => $metal_voucher_detail) {
-               ?> 
-            
-            <td style="padding: 2px" class="">Qty</td>
-            <td style="padding: 2px" class="text-center"><?= ($metal_voucher_detail['quantity']) ?></td>
-            <?php 
-            }?>
-            </tr><tr><?php
-            foreach ($metal_voucher_details as $index => $metal_voucher_detail) {
-               ?> 
-            
-            <td style="padding: 2px" class="">Karat</td>
-            <td style="padding: 2px" class="text-center"><?php
-              if($metal_voucher_detail['purity']>=91.50 && $metal_voucher_detail['purity']<= 92){
-                    echo "22 Kt";
-              }elseif($metal_voucher_detail['purity']>=87.50 && $metal_voucher_detail['purity']<= 88){
-                    echo "21 Kt";
-              }elseif($metal_voucher_detail['purity']>=75 && $metal_voucher_detail['purity']<= 75.50){
-                    echo "18 Kt";
-              }elseif($metal_voucher_detail['purity']>=58 && $metal_voucher_detail['purity']<= 58.50){
-                    echo "14 Kt";
-              }elseif($metal_voucher_detail['purity']>=41 && $metal_voucher_detail['purity']<= 42){
-                    echo "10 Kt";
-              }
-
-            ?></td>
-            <?php 
-            }?>
-            </tr><tr><?php
-            foreach ($metal_voucher_details as $index => $metal_voucher_detail) {
-               ?> 
-            
-            <td style="padding: 2px" class="">Desc</td>
-            <td style="padding: 2px" class="text-center"><?= ($metal_voucher_detail['description']) ?></td>
-         <?php 
-            }
-          ?>
+          
       </table>
     </div>
   <?php //} 
