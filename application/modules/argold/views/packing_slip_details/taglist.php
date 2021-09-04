635e75bab0854f5f5811aaee6f7603636e9a3816
kiran <kiran1234>
2021-09-04 16:11:17 +0530

    change

--- application/modules/argold/views/packing_slip_details/taglist.php
+++ application/modules/argold/views/packing_slip_details/taglist.php
@@ -4,7 +4,7 @@
     ?>
       <!-- <h6>Packet No :<?=$packet_no ?> </h6>     -->
       <div class="" style="font-weight:500">
-      <table class="" width="100%"  style="font-size: 11px">
+      <table class="" width="50%"  style="font-size: 11px">
             <?php foreach ($metal_voucher_details as $index => $metal_voucher_detail) {
               
                    ?>
