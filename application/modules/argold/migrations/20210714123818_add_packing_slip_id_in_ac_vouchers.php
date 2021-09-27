be1b498b2e769904fe18310db5117056b5b8e705
kiran <kiran1234>
2021-09-27 17:28:55 +0530

    change

--- application/modules/argold/migrations/20210714123818_add_packing_slip_id_in_ac_vouchers.php
+++ application/modules/argold/migrations/20210714123818_add_packing_slip_id_in_ac_vouchers.php
@@ -6,8 +6,8 @@ class Migration_add_packing_slip_id_in_ac_vouchers extends CI_Model {
 
   public function up()
   {
-    $this->db->query("ALTER TABLE `ac_vouchers` ADD `packing_slip_id` int(11) NULL DEFAULT '0'");
-    $this->db->query("ALTER TABLE `ac_ledger` ADD `packing_slip_id` int(11) NULL DEFAULT '0'");
+    // $this->db->query("ALTER TABLE `ac_vouchers` ADD `packing_slip_id` int(11) NULL DEFAULT '0'");
+    // $this->db->query("ALTER TABLE `ac_ledger` ADD `packing_slip_id` int(11) NULL DEFAULT '0'");
   }
 
 
