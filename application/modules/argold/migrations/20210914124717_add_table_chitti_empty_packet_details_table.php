d0f14f1803fe51ddfd7327ac911dd93aee4fea9c
kiran <kiran1234>
2021-10-04 12:40:52 +0530

    chaneg

--- application/modules/argold/migrations/20210914124717_add_table_chitti_empty_packet_details_table.php
+++ application/modules/argold/migrations/20210914124717_add_table_chitti_empty_packet_details_table.php
@@ -6,18 +6,18 @@ class Migration_add_table_chitti_empty_packet_details_table extends CI_Model {
 
   public function up()
   {
-    $this->db->query("CREATE TABLE `chitti_empty_packet_details` (
-											  `id` int(11) NOT NULL AUTO_INCREMENT,
-											  `weight` int(11) DEFAULT NULL,
-											  `quantity` varchar(45) NOT NULL,
-											  `chitti_id` int(11) DEFAULT '0',
-											  `created_at` datetime NOT NULL,
-											  `updated_at` datetime NOT NULL,
-											  `is_delete` tinyint(1) DEFAULT '0',
-											  `created_by` int(11) DEFAULT '0',
-											  `updated_by` int(11) DEFAULT '0',
-											  PRIMARY KEY (`id`)
-											) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
+    // $this->db->query("CREATE TABLE `chitti_empty_packet_details` (
+				// 							  `id` int(11) NOT NULL AUTO_INCREMENT,
+				// 							  `weight` int(11) DEFAULT NULL,
+				// 							  `quantity` varchar(45) NOT NULL,
+				// 							  `chitti_id` int(11) DEFAULT '0',
+				// 							  `created_at` datetime NOT NULL,
+				// 							  `updated_at` datetime NOT NULL,
+				// 							  `is_delete` tinyint(1) DEFAULT '0',
+				// 							  `created_by` int(11) DEFAULT '0',
+				// 							  `updated_by` int(11) DEFAULT '0',
+				// 							  PRIMARY KEY (`id`)
+				// 							) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
   }
 
 
