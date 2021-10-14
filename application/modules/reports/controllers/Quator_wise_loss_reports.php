73e0330479dc75e753c1d9c74ab672f8b628a616
kiran <kiran1234>
2021-10-14 15:35:21 +0530

    chaneg

--- application/modules/reports/controllers/Quator_wise_loss_reports.php
+++ application/modules/reports/controllers/Quator_wise_loss_reports.php
@@ -20,21 +20,22 @@ class Quator_wise_loss_reports extends BaseController {
     $quator_details= $this->quator_model->find('name,from_date,to_date,MONTH(from_date) as from_month,MONTH(to_date) as to_month,YEAR(from_date) as from_year,YEAR(to_date) as to_year',array('name'=>$this->data['quator_name']));
 
 
-    $this->data['work_details']=$this->ledger_model->find('IFNULL(sum(debit_amount),0) - IFNULL(sum(credit_amount),0) as amount',
+    
+    $this->data['trial_balance']=array();
+    if(!empty($this->data['quator_name'])){
+      $this->data['work_details']=$this->ledger_model->find('IFNULL(sum(debit_amount),0) - IFNULL(sum(credit_amount),0) as amount',
                array('(purity != factory_purity or (account_name in ("EXPORT ACCOUNT", "EXPORT DIFF.") and voucher_type = "metal issue voucher")
                 )'=>NULL,
                 'account_name !='=>"VADOTAR",'MONTH(voucher_date) in ('.$quator_details['from_month'].','.$quator_details['to_month'].')'=>NULL,'YEAR(voucher_date) in ('.$quator_details['from_year'].','.$quator_details['to_year'].')'=>NULL));
 
-    $this->data['trial_balance']=array();
-    if(!empty($this->data['quator_name'])){
       $where['where']=array('date(voucher_date) >='=>$quator_details['from_date'],'date(voucher_date) <='=>$quator_details['to_date'],'receipt_type!='=>'Unrecovarable','account_name!='=>'Loss Account');
 
-    $select = "account_name, 
+      $select = "account_name, 
                IFNULL((sum(debit_weight*purity)/100),0) - IFNULL((sum(credit_weight*factory_purity)/100),0) as fine,
                IFNULL(sum((purity-factory_purity)*debit_weight/100),0) - IFNULL(sum((factory_purity-purity)*credit_weight/100),0) as vadotar,
                IFNULL(sum(debit_amount),0) - IFNULL(sum(credit_amount),0) as amount,
                IFNULL(sum(usd_debit_amount),0) - IFNULL(sum(usd_credit_amount),0) as usd_amount,0 as id";
-    $this->data['trial_balance'] = $this->model->get($select,$where, array() , 
+      $this->data['trial_balance'] = $this->model->get($select,$where, array() , 
                                                       array('group_by'=>'account_name,',
                                                             'order_by'=>'account_name asc'));
     }
