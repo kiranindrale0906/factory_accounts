8f95f7ba275f26ff3a283345790a2fdfaccc6c71
kiran <kiran1234>
2021-10-14 14:13:06 +0530

    change

--- application/modules/reports/views/quator_wise_loss_reports/index.php
+++ application/modules/reports/views/quator_wise_loss_reports/index.php
@@ -206,8 +206,8 @@
     </div>
   </div>  
 </div>
-<div class="bold">Total Loss : <?=$total_loss=four_decimal($liabilities_total-$assets_total); ?></div>
 <div class="row">
+<div class="bold">Total Loss : <?=$total_loss=four_decimal($liabilities_total-$assets_total); ?></div>
   <div class="col-md-6">
     <div class="form-group container">
       <div class="table-responsive">          
