1d86e86761692ebbb6ea9ee6c4f7ef161b5c0a33
kiran <kiran1234>
2021-11-11 15:14:57 +0530

    chaneg

--- application/modules/reports/views/trial_balances/trial_balance.php
+++ application/modules/reports/views/trial_balances/trial_balance.php
@@ -19,7 +19,6 @@
               $liabilities_usd_amount = 0;
               if(!empty($trial_balance_records)) {
                 foreach ($trial_balance_records as $record) {
-                  echo"<pre>12";print_r($record['unrecoverable_account_name']);
                   if (   ($record['fine'] <= 0
                           && $record['account_name'] != 'VADOTAR')
                       || ($record['account_name'] == 'Tounch Loss Fine')) continue;
