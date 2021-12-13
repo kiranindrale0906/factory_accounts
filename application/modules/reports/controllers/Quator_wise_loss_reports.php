54feeda1eaf0abe63c85cec8e670e5a95e7a0425
kiran <kiran1234>
2021-12-13 16:48:05 +0530

    change

--- application/modules/reports/controllers/Quator_wise_loss_reports.php
+++ application/modules/reports/controllers/Quator_wise_loss_reports.php
@@ -133,9 +133,9 @@ class Quator_wise_loss_reports extends BaseController {
         }else{
           $ghiss_melting_loss=array();
           $url=API_ARG_PATH."issue_and_receipts/loss_report_for_accounts/index";
+          pd($data);
           $jan2021_records=json_decode(curl_post_request($url,$data),true);
           $records=!empty($jan2021_records)?$jan2021_records['data']['loss_details']['loss_detail']:$jan2021_records['data']['loss_details']['loss_detail']=array();
-          pd($jan2021_records);
           $ghiss_melting_loss=$this->voucher_model->get('description,site_name,credit_weight as in_weight,purity as in_lot_purity,argold_id as parent_id,0 as out_weight', array('account_name'=>'Loss Account','site_name'=>'AR Gold','receipt_type'=>'Ghiss Melting Loss','quator'=>$data['quator']),array());
           foreach ($ghiss_melting_loss as $ghiss_melting_loss_index => $ghiss_melting_loss_value) {
           $data['issue_department_id']=$ghiss_melting_loss_value['parent_id'];
