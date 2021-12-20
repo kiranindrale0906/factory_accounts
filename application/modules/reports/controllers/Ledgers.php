af5636baf4e9d9ee0fc6db904e4bc32c0fff2d09
System Administrator <root@Atuls-MacBook-Pro.local>
2021-12-20 17:40:24 +0530

    changs;

--- application/modules/reports/controllers/Ledgers.php
+++ application/modules/reports/controllers/Ledgers.php
@@ -58,6 +58,9 @@ class Ledgers extends BaseController {
                 )'] = NULL;
       } elseif ($this->data['domestic_export'] == 'Domestic') {
         $where['purity != factory_purity'] = NULL;
+      } elseif ($this->data['domestic_export'] == 'Tanishq') {
+        $where['account_name'] = 'Tanishq';
+        $where['voucher_type'] = 'metal issue voucher';
       } elseif ($this->data['domestic_export'] == 'Export') {
         $where['(    account_name in ("'.$export_account_names.'") 
                  and voucher_type = "metal issue voucher")'] = NULL;
