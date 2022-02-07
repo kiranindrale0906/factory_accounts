1cc8f1ea00262ac7ccf7deb34d0be5338ee567be
kiran <kiran1234>
2022-02-07 15:55:58 +0530

    chaneg

--- application/modules/argold/views/chittis/view.php
+++ application/modules/argold/views/chittis/view.php
@@ -50,13 +50,10 @@
   <div class="container" style="display: flex; height: 'auto'; align-items: center; justify-content: center;">
         <div style="width: 'auto'">
           <?php 
-            $string=$qr_code_detail['id'];      
+            $string=$record['id'];      
             $qr_code = generate_qrcode($string,'48');
             echo $qr_code;
           ?>
-        </div>
-        <div style="font-size: 7px; font-weight: bold; font-family: Helvetica">
-          <?php echo 'Chitti No - '.$qr_code_detail['id']; ?>
         </div>
         
 </div>
