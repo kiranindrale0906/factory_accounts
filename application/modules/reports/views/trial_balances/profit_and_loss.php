e5dc5257b5ab0642e404ed1b13d3803fff7a6e1c
System Administrator <root@Atuls-MacBook-Pro.local>
2022-03-13 20:45:11 +0530

    changes

--- application/modules/reports/views/trial_balances/profit_and_loss.php
+++ application/modules/reports/views/trial_balances/profit_and_loss.php
@@ -31,7 +31,7 @@
   $purchase_export_rate = !empty($domestic_export_records['PURCHASE ACCOUNT'][1]['fine']) ? -1 * $domestic_export_records['PURCHASE ACCOUNT'][1]['amount'] / $domestic_export_records['PURCHASE ACCOUNT'][1]['fine'] : 0;
   $purchase_export_amount = !empty($domestic_export_records['PURCHASE ACCOUNT'][1]['amount']) ? -1 * $domestic_export_records['PURCHASE ACCOUNT'][1]['amount'] : 0;
 
-  $domestic_export_records['SALES ACCOUNT'][0]['amount'] = $domestic_export_records['SALES ACCOUNT'][0]['amount'] ?? 0
+  $domestic_export_records['SALES ACCOUNT'][0]['amount'] = $domestic_export_records['SALES ACCOUNT'][0]['amount'] ?? 0;
   $domestic_export_records['SALES ACCOUNT'][0]['amount'] -= 401904347.230;
 
   $domestic_export_records['SALES ACCOUNT'][0]['fine'] == $domestic_export_records['SALES ACCOUNT'][0]['fine'] ?? 0;
