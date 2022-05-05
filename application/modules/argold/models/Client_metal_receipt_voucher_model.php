7d39af2b5240ec0b8a1546bc456875a72e422343
kiran1234 <kiran1234>
2022-05-05 19:58:50 +0530

    change

--- application/modules/argold/models/Client_metal_receipt_voucher_model.php
+++ application/modules/argold/models/Client_metal_receipt_voucher_model.php
@@ -524,6 +524,8 @@ class Client_metal_receipt_voucher_model extends Core_metal_receipt_voucher_mode
       $api_url = API_ARC_PATH.$api_url;
     elseif ($attributes['account_name'] == 'Export Internal Software')
       $api_url = API_EXPORT_INTERNAL_PATH.$api_url;
+
+    pd($api_url);
     $result = curl_post_request($api_url, $send_data);
   }
 
