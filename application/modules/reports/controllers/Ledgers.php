132511cb3b5911a51155bd45c7b8991435c0895f
kiran1234 <kiran1234>
2022-05-06 20:45:08 +0530

    cahneg

--- application/modules/reports/controllers/Ledgers.php
+++ application/modules/reports/controllers/Ledgers.php
@@ -169,16 +169,20 @@ class Ledgers extends BaseController {
       $voucher_id=explode(',', $issue_value['voucher_id']);
       $ac_voucher_issue_detail=$this->voucher_model->get('metal_receipt_voucher_reference_id,id',array('where_in'=>array('id'=>$voucher_id),'where'=>array('metal_receipt_voucher_reference_id is not NULL'=>NULL)));
       $metal_receipt_voucher_reference_id=array_column($ac_voucher_issue_detail,'metal_receipt_voucher_reference_id');
+      if(!empty($metal_receipt_voucher_reference_id)){
       $reference_ac_voucher_issue_detail=$this->voucher_model->find('GROUP_CONCAT(DISTINCT(account_name)) as account_name',array('where_in'=>array('id'=>$metal_receipt_voucher_reference_id)));
       $issues[$issue_index]['reference_account_name']=$reference_ac_voucher_issue_detail['account_name'];
+      }
     }
     $receipts = $this->ledger_model->get($receipt_issue_select, $where_receipt, array(), array('order_by'=>'parent_id, voucher_type, str_voucher_date asc', 'group_by' => $this->data['group']));
     foreach ($receipts as $receipt_index => $receipt_value) {
       $voucher_id=explode(',', $receipt_value['voucher_id']);
       $ac_voucher_receipt_detail=$this->voucher_model->get('metal_receipt_voucher_reference_id',array('where_in'=>array('id'=>$voucher_id),'where'=>array('metal_receipt_voucher_reference_id is not NULL'=>NULL)));
       $metal_receipt_voucher_reference_id=array_column($ac_voucher_receipt_detail,'metal_receipt_voucher_reference_id');
+      if(!empty($metal_receipt_voucher_reference_id)){
       $reference_ac_voucher_receipt_detail=$this->voucher_model->find('GROUP_CONCAT(DISTINCT(account_name)) as account_name',array('where_in'=>array('id'=>$metal_receipt_voucher_reference_id)));
       $receipts[$receipt_index]['reference_account_name']=$reference_ac_voucher_receipt_detail['account_name'];
+    }
     }
 
      $domestic_export_receipt_issue_select='account_name,voucher_date,date_format(voucher_date,"%Y-%m-%d") as str_voucher_date,((debit_weight*purity)/100)- 
