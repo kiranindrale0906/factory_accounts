89811a1272f533b5f0232b4e69c6aa5f1f0d0cc2
System Administrator <root@Atuls-MacBook-Pro.local>
2022-08-13 01:34:33 +0530

    changes

--- application/modules/argold/models/Client_metal_receipt_voucher_model.php
+++ application/modules/argold/models/Client_metal_receipt_voucher_model.php
@@ -587,6 +587,7 @@ class Client_metal_receipt_voucher_model extends Core_metal_receipt_voucher_mode
       if (empty($record['weight'])) return;
       if(empty($metal_receipt_voucher['debit_weight'])
          || ($metal_receipt_voucher['debit_weight'] != $record['weight'])) {
+        pd($metal_receipt_voucher, 0); pd($record);
         $metal_receipt_obj = new metal_receipt_voucher_model($data);
         $metal_receipt_obj->before_validate();
         $metal_receipt_obj->save();
