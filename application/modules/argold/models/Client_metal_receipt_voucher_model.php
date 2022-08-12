ced4151b4175ec1f581be9d161465864d7cf5c5d
System Administrator <root@Atuls-MacBook-Pro.local>
2022-08-13 01:32:20 +0530

    changes

--- application/modules/argold/models/Client_metal_receipt_voucher_model.php
+++ application/modules/argold/models/Client_metal_receipt_voucher_model.php
@@ -590,6 +590,7 @@ class Client_metal_receipt_voucher_model extends Core_metal_receipt_voucher_mode
         $metal_receipt_obj = new metal_receipt_voucher_model($data);
         $metal_receipt_obj->before_validate();
         $metal_receipt_obj->save();
+        lq();
       } 
     }
   }
