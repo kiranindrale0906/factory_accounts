512f0c62af0e59196a5004d8e191d3037ad35d10
System Administrator <root@Atuls-MacBook-Pro.local>
2022-08-13 10:54:33 +0530

    chnages

--- application/modules/reports/views/trial_balances/alloy_vodator_balance.php
+++ application/modules/reports/views/trial_balances/alloy_vodator_balance.php
@@ -1,18 +1,12 @@
-<?php 
-  foreach($receipt_types as $receipt_type) {
-    foreach($site_names as $site_name) {
-      foreach($hostversions as $hostversion) {
-?>
-    
-
 <div class="col-md-12">
   <div class="form-group container">
-    <h5 class="ml-2 pl-2"><?= $receipt_type ?></h5>
+    <h5 class="ml-2 pl-2"></h5>
     <div class="table-responsive m-t-20">
       <table class="table table-sm fixedthead table-default">
         <thead>
           <tr>
             <th>Stock</th>
+            <th>Type</th>
             <th class="text-right">Accounts</th>
             <th class="text-right">Accounts Fine</th>
             <th class="text-right">Factory</th>
@@ -21,23 +15,28 @@
             <th class="text-right">Total Fine</th>
           </tr>
         </thead>
-        <tr>
-          <td><?= $site_name.' '.$hostversion ?></td>
-            <td class="text-right"><?= four_decimal($account_vadotar_balance[$receipt_type][$site_name][$hostversion]['balance']) ?></td>
-            <td class="text-right"><?= four_decimal($account_vadotar_balance[$receipt_type][$site_name][$hostversion]['balance_fine']) ?></td>
-            <td class="text-right"><?= four_decimal($factory_vadotar_records[$receipt_type][$site_name][$hostversion]['balance']) ?></td>
-            <td class="text-right"><?= four_decimal($factory_vadotar_records[$receipt_type][$site_name][$hostversion]['balance_fine']) ?></td>
-            <td class="text-right"><?= four_decimal($account_vadotar_balance[$receipt_type][$site_name][$hostversion]['balance'] - $factory_vadotar_records[$receipt_type][$site_name][$hostversion]['balance']) ?></td>
-            <td class="text-right"><?= four_decimal($factory_vadotar_records[$receipt_type][$site_name][$hostversion]['balance'] - $account_vadotar_balance[$receipt_type][$site_name][$hostversion]['balance']) ?></td>
-        </tr>
-        
+        <?php 
+          foreach($receipt_types as $receipt_type) {
+            foreach($site_names as $site_name) {
+              foreach($hostversions as $hostversion) {
+        ?>
+                <tr>
+                  <td><?= $site_name.' '.$hostversion ?></td>
+                    <td><?= $receipt_type ?></td>
+                    <td class="text-right"><?= four_decimal($account_vadotar_balance[$receipt_type][$site_name][$hostversion]['balance']) ?></td>
+                    <td class="text-right"><?= four_decimal($account_vadotar_balance[$receipt_type][$site_name][$hostversion]['balance_fine']) ?></td>
+                    <td class="text-right"><?= four_decimal($factory_vadotar_records[$receipt_type][$site_name][$hostversion]['balance']) ?></td>
+                    <td class="text-right"><?= four_decimal($factory_vadotar_records[$receipt_type][$site_name][$hostversion]['balance_fine']) ?></td>
+                    <td class="text-right"><?= four_decimal($account_vadotar_balance[$receipt_type][$site_name][$hostversion]['balance'] - $factory_vadotar_records[$receipt_type][$site_name][$hostversion]['balance']) ?></td>
+                    <td class="text-right"><?= four_decimal($factory_vadotar_records[$receipt_type][$site_name][$hostversion]['balance'] - $account_vadotar_balance[$receipt_type][$site_name][$hostversion]['balance']) ?></td>
+                </tr>
+        <?php  
+              }
+            }
+          }
+        ?>
       </table>
     </div>
   </div>
 </div>
 
-<?php  
-      }
-    }
-  }
-?>
