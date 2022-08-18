b98d691bf19059263c8ae3e697a864ebadc4c0b5
kiran1234 <kiran1234>
2022-08-18 19:36:47 +0530

    chng

--- application/modules/reports/controllers/Ledgers.php
+++ application/modules/reports/controllers/Ledgers.php
@@ -186,11 +186,11 @@ class Ledgers extends BaseController {
         $where_receipt=array('(account_name = ("Export Internal Software")  and receipt_type="Export Internal" 
                  and voucher_type = "metal receipt voucher") and (debit_weight != 0 or debit_amount != 0)' => NULL);
         if ($this->data['site_name'] == 'ARF' || $this->data['site_name'] == 'ARF (May 2022)'|| $this->data['site_name'] == 'ARF (Aug 2022)')
-          $where_receipt['narration'] = $this->data['site_name'];
+          $where_receipt['description'] = 'ARF Software';
         elseif ($this->data['site_name'] == 'ARC' || $this->data['site_name'] == 'ARC (May 2022)'|| $this->data['site_name'] == 'ARC (Aug 2022)') 
-          $where_receipt['narration'] = $this->data['site_name'];
+          $where_receipt['description'] = 'ARC Software';
         elseif ($this->data['site_name'] == 'AR Gold' || $this->data['site_name'] == 'AR Gold (May 2022)'|| $this->data['site_name'] == 'AR Gold (Aug 2022)')
-          $where_receipt['narration'] = $this->data['site_name'];  
+          $where_receipt['description'] = 'AR Gold Software';    
     }
     
     $issues   = $this->ledger_model->get($receipt_issue_select, $where_issue,   array(), array('order_by'=>'chitti_id, voucher_type, str_voucher_date asc', 'group_by' => $this->data['group']));
