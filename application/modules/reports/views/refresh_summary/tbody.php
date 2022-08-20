d998bba2ccce53f0dc9b5452dfc684c213fd6760
System Administrator <root@Atuls-MacBook-Pro.local>
2022-08-20 12:57:27 +0700

    changes

--- application/modules/reports/views/refresh_summary/tbody.php
+++ application/modules/reports/views/refresh_summary/tbody.php
@@ -2,7 +2,14 @@
   <tr>
     <td><?= $record['created_at'] ?></td>
     <td>
-      <?= $record['item_name'] ?> <?=$record['refresh_id']?>
+      <?= $record['item_name'] ?> 
+      <?php 
+        $refresh_ids = explode(",", $record['refresh_id'])
+        foreach ($refresh_ids as $refresh_id) { ?>
+          <a href='<?= base_url() ?>argold/voucher_details/view/<?=$refresh_id?>'><?= $refresh_id ?></a>
+          <?php 
+        } 
+      ?>    
     </td>
     <td><?= four_decimal($record['factory_purity']) ?></td>
     <td><?= four_decimal($record['purity']) ?></td>
