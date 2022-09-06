3cc984ad6ed1b9c2b8cf360fc96af9f3927d68cc
kiran1234 <kiran1234>
2022-09-06 20:01:51 +0530

    chng

--- application/modules/argold/models/Client_metal_receipt_voucher_model.php
+++ application/modules/argold/models/Client_metal_receipt_voucher_model.php
@@ -523,10 +523,11 @@ class Client_metal_receipt_voucher_model extends Core_metal_receipt_voucher_mode
       $api_data = array_merge($api_data, array('type' => 'Pure','description' => $api_data['description'].'-'.$attributes['site_name']));
       $send_data['internal_receipts'] = $api_data;
       if($attributes['account_name'] == 'Domestic Internal Software'){
-      $send_data['domestic_internal_receipts'] = $api_data;
-      $api_url = "api/api_domestic_internal_receipts/store";
+        $api_data['account']=$attributes['site_name'];
+        $send_data['domestic_internal_receipts'] = $api_data;
+        $api_url = "api/api_domestic_internal_receipts/store";
       }else{
-      $api_url = "api/api_internal_receipts/store";
+        $api_url = "api/api_internal_receipts/store";
       }
 
     }elseif (   $attributes['receipt_type'] == 'AR Gold RND'
