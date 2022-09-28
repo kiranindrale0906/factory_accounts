ce7074538b9485c97ef4b4f6175c33a69a1b8ad8
System Administrator <root@Atuls-MacBook-Pro.local>
2022-09-28 22:55:07 +0530

    changes

--- application/modules/api/controllers/Api_chittis.php
+++ application/modules/api/controllers/Api_chittis.php
@@ -6,18 +6,22 @@ class Api_chittis extends Chittis {
   public function __construct() {
     parent::__construct();
   }
+
   public function index() {
-    if(!empty($_POST['access_token']) && $_POST['access_token']==API_ACCESS_TOKEN){
-    $chitti_details = []; 
-    $chitti_details['chittis']=$this->api_chitti_model->find('',array('id'=>$_POST['chitti_id']));
-    $chitti_details['chitti_details']=$this->voucher_model->get('',array('chitti_id'=>$_POST['chitti_id'],'voucher_type'=>"metal issue voucher"));
-    
-    echo json_encode(array('data'    =>$chitti_details,
-                         'status'      => 'success',
-                         'open_modal'  => FALSE));die;
-  }else{
-    echo json_encode(array('status'      => 'error',
-                         'open_modal'  => FALSE));die;
+    if (   !empty($_POST['access_token']) 
+        && $_POST['access_token']==API_ACCESS_TOKEN) {
+      $chitti_details = []; 
+      $chitti_details['chittis'] = $this->api_chitti_model->find('',array('id'=>$_POST['chitti_id']));
+      $chitti_details['chitti_details'] = $this->voucher_model->get('sum(fine) as fine,sum(rate) as rate,sum(factory_fine) as factory_fine,sum(credit_weight) as credit_weight,group_concat(narration) as narration,purity,chitti_purity,factory_purity,customer_name',
+                                                                      array('chitti_id'=>$_POST['chitti_id'], 
+                                                                            'voucher_type'=>"metal issue voucher"), array(), array('group_by' => 'item_code'));
+      echo json_encode(array('data'    =>$chitti_details,
+                             'status'      => 'success',
+                             'open_modal'  => FALSE));
+    } else {
+      echo json_encode(array('status'      => 'error',
+                             'open_modal'  => FALSE));
+    }
+    die;
   }
-}
 }
