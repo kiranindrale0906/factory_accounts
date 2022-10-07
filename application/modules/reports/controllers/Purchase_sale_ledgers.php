1acb82c08543756b7e12635e13be7717e291ed19
kiran1234 <kiran1234>
2022-10-07 13:34:36 +0530

    chng

---
+++ application/modules/reports/controllers/Purchase_sale_ledgers.php
@@ -0,0 +1,34 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+include_once APPPATH . "modules/reports/controllers/Ledgers.php";
+class Purchase_sale_ledgers extends Ledgers {
+
+  public function __construct() {
+    parent::__construct();
+    $this->load->model(array('masters/account_model', 'transactions/ledger_model'));
+  }
+
+  public function index() {
+    $this->data['report_type'] = 'Purchase Sales Ledger';
+    $this->_get_form_data();
+    $this->load->render($this->router->class."/index",$this->data);
+  }
+
+  public function create() {
+    ini_set('max_execution_time', '0');
+    $this->ledger_model->regenerate_ledger_records();
+  }
+
+  public function _get_form_data() {
+    $this->data['voucher_dates']=array();
+    $this->data['account_names'] = $this->account_model->get('distinct(ac_account.name) as name, ac_account.id as id',
+                                                              array('where' => array('ac_account.name in ("Purchase Account","Sales Account")' =>NULL)),
+                                                              array(),
+                                                              array('order_by' => 'ac_account.name asc'));
+    $account_id = (!empty($_GET['account_ledgers']['account_id'])) ? $_GET['account_ledgers']['account_id'] : 0;
+    $this->data['account_id'] = $account_id;  
+    if ($this->data['account_id'] != 0)
+      $this->get_datewise_ledger_records();
+  }
+}
