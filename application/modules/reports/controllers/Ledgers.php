70b5b734d0833c4e986bf4e684b9658443499b6a
kiran1234 <kiran1234>
2022-10-07 16:28:28 +0530

    chng

--- application/modules/reports/controllers/Ledgers.php
+++ application/modules/reports/controllers/Ledgers.php
@@ -222,7 +222,7 @@ class Ledgers extends BaseController {
     }
 
     if ($this->data['report_type'] == 'Purchase Sales Ledger') {
-      $where_issue['ac_ledger.sale_type']="Sale";
+      $where_issue['chitties.sale_type']="Sale";
       $receipt_issue_select .=',chitties.account_name as chitti_account_name';
       $issues   = $this->ledger_model->get($receipt_issue_select, $where_issue,   array(array('chitties','chitties.id=ac_ledger.chitti_id')), array('order_by'=>'ac_ledger.chitti_id, ac_ledger.voucher_type, ac_ledger.str_voucher_date asc', 'group_by' => $this->data['group']));
   
@@ -244,7 +244,7 @@ class Ledgers extends BaseController {
       }
     }
     if ($this->data['report_type'] == 'Purchase Sales Ledger') {
-      $where_receipt['ac_ledger.sale_type']="Sale";
+      $where_receipt['chitties.sale_type']="Sale";
       $receipt_issue_select .=',chitties.account_name as chitti_account_name';
        $receipts = $this->ledger_model->get($receipt_issue_select, $where_receipt, array(array('chitties','chitties.id=ac_ledger.chitti_id')), array('order_by'=>'ac_ledger.parent_id, ac_ledger.voucher_type, ac_ledger.str_voucher_date asc', 'group_by' => $this->data['group']));
     
