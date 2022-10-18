fdd4a963b991eba1fcd25111b0d35a0dadc00ea5
kiran1234 <kiran1234>
2022-10-18 12:43:51 +0530

    chng

--- application/modules/reports/views/account_ledgers/index.php
+++ application/modules/reports/views/account_ledgers/index.php
@@ -1,4 +1,5 @@
 <div class="col-md-12">
+  <?php if(!empty($_GET['limit_date']) && $_GET['limit_date']==1)?>
   <?= getHttpButton('Generate Ledger (4 to 6 month)', base_url().'reports/account_ledgers/create?limit_date=1', 'float-right btn-success ml-5'); ?>
   <?= getHttpButton('Generate Ledger (6 to 9 month)', base_url().'reports/account_ledgers/create?limit_date=2', 'float-right btn-success ml-5'); ?>
   <?= getHttpButton('Generate Ledger (9 to current month)', base_url().'reports/account_ledgers/create?limit_date=3', 'float-right btn-success ml-5'); ?>
