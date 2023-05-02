56e52f32cc8c7d3ef3cde0afd6c1b822430c259f
kiran1234 <kiran1234>
2023-05-02 19:50:18 +0530

    cgng

--- application/modules/argold/models/Client_metal_receipt_voucher_model.php
+++ application/modules/argold/models/Client_metal_receipt_voucher_model.php
@@ -334,7 +334,7 @@ class Client_metal_receipt_voucher_model extends Core_metal_receipt_voucher_mode
   private function set_metal_issue_voucher_attributes_from_receipt_type_for_stone() {
     if ($this->attributes['receipt_type'] == "Stone") {
       unset($this->formdata['metal_issue_vouchers']);
-      $this->formdata['metal_issue_vouchers'] = array(array('account_name' => 'ARC Software (Aug 2022)',
+      $this->formdata['metal_issue_vouchers'] = array(array('account_name' => 'ARC Software (Apr 2023)',
                                                             'credit_weight' => $this->attributes['debit_weight'],
                                                             'purity' => $this->attributes['purity'],
                                                             'fine' => $this->attributes['debit_weight'] * $this->attributes['purity'] / 100,
