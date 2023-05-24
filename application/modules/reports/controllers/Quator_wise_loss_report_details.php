53596ddc358ea01af3bb4bb27b146c9a4106e90d
kiran1234 <kiran1234>
2023-05-24 19:20:29 +0530

    chng

--- application/modules/reports/controllers/Quator_wise_loss_report_details.php
+++ application/modules/reports/controllers/Quator_wise_loss_report_details.php
@@ -172,7 +172,7 @@ class Quator_wise_loss_report_details extends Ledgers {
                 $ghiss_details=json_decode(curl_post_request($url,$data),true);
                  $out_weight=!empty($ghiss_details)&&!empty($ghiss_details['data']['ghiss_melting_out_weights'])?$ghiss_details['data']['ghiss_melting_out_weights']:0;
                 $ghiss_melting_loss[$ghiss_melting_loss_index]['out_weight']=$out_weight;
-          } $fire_tounch_loss=$this->voucher_model->get('receipt_type,description,site_name,credit_weight as in_weight,purity as in_lot_purity,argold_id as parent_id,0 as out_weight,created_at,created_at as first_date,created_at as last_date', array('account_name'=>'ARF Loss Account (Apr 2023)','site_name'=>'ARF (Apr 2023)','date(created_at)>='=>'2021-03-13','receipt_type'=>'Ghiss Melting Loss','quator'=>$data['quator'],'description'=>$_GET['category']),array());
+          } $fire_tounch_loss=$this->voucher_model->get('receipt_type,description,site_name,credit_weight as in_weight,purity as in_lot_purity,argold_id as parent_id,0 as out_weight,created_at,created_at as first_date,created_at as last_date', array('account_name'=>'ARF Loss Account (Apr 2023)','site_name'=>'ARF (Apr 2023)','date(created_at)>='=>'2021-03-13','receipt_type'=>'Fire Tounch Loss','quator'=>$data['quator'],'description'=>$_GET['category']),array());
           foreach ($fire_tounch_loss as $fire_tounch_loss_index => $fire_tounch_loss_value) {
                 $data['issue_department_id']=$fire_tounch_loss_value['parent_id'];
                 // $data['quator']=$this->data['quator_name'];
@@ -228,7 +228,7 @@ class Quator_wise_loss_report_details extends Ledgers {
          $out_weight=!empty($ghiss_details)&&!empty($ghiss_details['data']['ghiss_melting_out_weights'])?$ghiss_details['data']['ghiss_melting_out_weights']:0;
         $ghiss_melting_loss[$ghiss_melting_loss_index]['out_weight']=$out_weight;
     }
-   $fire_tounch_loss=$this->voucher_model->get('receipt_type,description,site_name,credit_weight as in_weight,purity as in_lot_purity,argold_id as parent_id,0 as out_weight,created_at,created_at as first_date,created_at as last_date', array('account_name'=>'AR Gold Loss Account (Apr 2023)','site_name'=>'AR Gold (Apr 2023)','date(created_at)>='=>'2021-03-13','receipt_type'=>'Ghiss Melting Loss','quator'=>$data['quator'],'description'=>$_GET['category']),array());
+   $fire_tounch_loss=$this->voucher_model->get('receipt_type,description,site_name,credit_weight as in_weight,purity as in_lot_purity,argold_id as parent_id,0 as out_weight,created_at,created_at as first_date,created_at as last_date', array('account_name'=>'AR Gold Loss Account (Apr 2023)','site_name'=>'AR Gold (Apr 2023)','date(created_at)>='=>'2021-03-13','receipt_type'=>'Fire Tounch Loss','quator'=>$data['quator'],'description'=>$_GET['category']),array());
     foreach ($fire_tounch_loss as $fire_tounch_loss_index => $fire_tounch_loss_value) {
         $data['issue_department_id']=$fire_tounch_loss_value['parent_id'];
         // $data['quator']=$this->data['quator_name'];
