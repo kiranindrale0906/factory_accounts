66e3d5fda464d27f4f6dfbca24138edabe1fc8a4
www-data <www-data@ip-172-26-11-156.ca-central-1.compute.internal>
2023-08-07 09:08:38 +0000

    chnage

--- application/modules/argold/views/refresh/view.php
+++ application/modules/argold/views/refresh/view.php
@@ -1,5 +1,5 @@
 <h5 class="heading noprint">Refresh View</h5>
-<a href="<?=ADMIN_PATH.'argold/refresh/'.$refresh['id']?>" class='btn bg_red white'>Delete</a>
+<a href="<?=ADMIN_PATH.'argold/refresh/delete/'.$refresh['id']?>" class='btn bg_red white'>Delete</a>
 <?php
 
   $this->load->view('refresh_details/viewlist');
