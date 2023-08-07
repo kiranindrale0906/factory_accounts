11cc24c047f085b0e522c0662ba66fd3d1663e33
kiran1234 <kiran1234>
2023-08-07 14:33:01 +0530

    chng

--- application/modules/argold/views/refresh/view.php
+++ application/modules/argold/views/refresh/view.php
@@ -1,5 +1,8 @@
 <h5 class="heading noprint">Refresh View</h5>
+<div class="col-md-8 text-right">
 <a href="<?=ADMIN_PATH.'argold/refresh/'.$refresh['id']?>" class='btn bg_red white'>Delete</a>
+
+</div>
 <?php
 
   $this->load->view('refresh_details/viewlist');
