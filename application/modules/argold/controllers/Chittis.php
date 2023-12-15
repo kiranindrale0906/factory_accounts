a5b750c449c7e70ac28805288998b7926577a066
www-data <www-data@ip-172-26-11-156.ca-central-1.compute.internal>
2023-12-15 06:23:40 +0000

    chng

--- application/modules/argold/controllers/Chittis.php
+++ application/modules/argold/controllers/Chittis.php
@@ -43,7 +43,7 @@ class Chittis extends BaseController {
 
     if($this->data['group_by']==1) {
       if($this->data['item_code']==1){
-        $select='sum(fine) as fine,sum(rate) as rate,sum(factory_fine) as factory_fine,sum(credit_weight) as credit_weight,group_concat(narration) as narration,purity,chitti_purity,factory_purity,customer_name,group_concat(item_code) as item_code';
+        $select='sum(fine) as fine,sum(rate) as rate,sum(factory_fine) as factory_fine,sum(credit_weight) as credit_weight,group_concat(narration) as narration,purity,chitti_purity,factory_purity,customer_name,group_concat(distinct(item_code)) as item_code';
         $group_by = 'customer_name,chitti_purity,(factory_purity-chitti_purity),item_code';
       }else{
         $select='sum(fine) as fine,sum(rate) as rate,sum(factory_fine) as factory_fine,sum(credit_weight) as credit_weight,group_concat(narration) as narration,purity,chitti_purity,factory_purity,customer_name,item_code';
