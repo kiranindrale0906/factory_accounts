87417a1f3f8c22a4c1e88e5798ce22c5131da679
Kiran <Kiran>
2023-12-29 20:54:36 +0530

    chng

--- application/modules/argold/models/Chitti_erp_model.php
+++ application/modules/argold/models/Chitti_erp_model.php
@@ -15,5 +15,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       $this->load->model(array('transactions/cash_issue_voucher_model'));
       $this->cash_issue_voucher_model->create_cash_vouchers_for_chitti($this->attributes['id'], $metal_issue_vouchers['amount']);
     }
+    public function set_chitti_id_in_metal_issue_vouchers() {
+    $chittis=array();
+
+    if (!empty($this->formdata['chitti_details'])) {
+      $chitti_ids=array_column($this->formdata['chitti_details'], 'chitti_id');
+      $chitti_id_details=array();
+      foreach ($chitti_ids as $index => $chitti_id) {
+        $chittis=explode('-', $chitti_id);
+        $chitti_id_details[$index]['packet_no']=$chittis[0];
+        $chitti_id_details[$index]['erp_argold_id']=$chittis[1];
+      }
+      $packet_nos=array_column($chitti_id_details, 'packet_no');
+      $argold_ids=array_column($chitti_id_details, 'erp_argold_id');
+
+      $chitti_details = $this->voucher_model->get('', array('site_name' => $this->attributes['site_name'],
+                                                            'packet_no' => $packet_nos,
+                                                            'erp_argold_id' => $argold_ids,
+                                                            'account_name' => $this->attributes['account_name'],
+                                                            'purity' => $this->attributes['purity']));
+    } else 
+      $chitti_details = $this->voucher_model->get('', array('chitti_id' => $this->attributes['id']));
+    
+    
+    foreach ($chitti_details as $index => $chitti_detail) {
+      if (isset($chitti_detail['id'])) {
+        $voucher_obj = new voucher_model($chitti_detail);
+        $voucher_obj->attributes['chitti_id'] = $this->attributes['id'];
+        $voucher_obj->attributes['voucher_date'] = $this->attributes['date'];
+        $voucher_obj->update(false);
+      }
+    }
+  }
 
   }
