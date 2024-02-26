d6e788db2ce55c36f5a64748e48afccd0e92162b
Kiran <Kiran>
2024-02-26 18:18:03 +0530

    chng

--- application/modules/reports/views/purchase_labour_ledgers/index.php
+++ application/modules/reports/views/purchase_labour_ledgers/index.php
@@ -2,12 +2,10 @@
 <?php 
   $this->load->view('reports/ledgers/report_header', array('header' => 'Purchase Labour Ledger'));
   $this->load->view('ac_vouchers/ac_vouchers/company_error_message'); 
-  $this->load->view('reports/ledgers/account_name_search', array('url' => ADMIN_PATH."reports/purchase_labour_ledgers/index"));
+  /*$this->load->view('reports/ledgers/account_name_search', array('url' => ADMIN_PATH."reports/purchase_labour_ledgers/index"));*/
 ?>
 
 <?php 
-  if (!empty($record['account_id'])) {
-   // $this->load->view('reports/ledgers/search'); 
+    $this->load->view('reports/ledgers/search'); 
     $this->load->view('reports/ledgers/date_wise_ledger', array('report' => 'purchase labour ledger')); 
-  }
 ?>
