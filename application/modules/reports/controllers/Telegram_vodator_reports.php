faa406f81f6b0473e598349d2eb95dde96140c6d
Kiran <Kiran>
2024-05-17 15:28:43 +0530

    chnge

--- application/modules/reports/controllers/Telegram_vodator_reports.php
+++ application/modules/reports/controllers/Telegram_vodator_reports.php
@@ -1,17 +1,17 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Telegram_vadator_report extends BaseController {
+class Telegram_vodator_reports extends BaseController {
   public function __construct() {
     parent::__construct();
     $this->load->model(array('ac_vouchers/voucher_model'));
 
-    $this->bot = new \TelegramBot\Api\BotApi('7199566349:AAF-0evNjld0Jl6OhaGeuVamGY4g-yNDc6k');
+    // /$this->bot = new \TelegramBot\Api\BotApi('7199566349:AAF-0evNjld0Jl6OhaGeuVamGY4g-yNDc6k');
   }
 
   public function index() {
     $date = date('Y-m-d');
     $this->send_message($date);
-    $this->send_vadotar_records($date);
+    //$this->send_vadotar_records($date);
   }
   private function send_vadotar_records($date) {
     $vadotars = $this->voucher_model->get('date(voucher_date) voucher_date, sum(factory_fine - fine) as vadotar',array('date(voucher_date)' => $date));
@@ -24,7 +24,21 @@ class Telegram_vadator_report extends BaseController {
     //Bhaskar: 1316386536
     //Nikhil Ranawat: 1056863449
     //Bheru Sankhla: 1699299372
-
-    $this->bot->sendMessage('712491427', $message);  
+  
+    //$this->bot->sendMessage('712491427', $message);  
+    pd($message);
+    $text="Hello kiran";
+    $chat_id="1855495238";
+    $token="7199566349:AAF-0evNjld0Jl6OhaGeuVamGY4g-yNDc6k";
+    $url="https://api.telegram.org/bot$token/sendMessage?text=$text&chat_id=$chat_id";
+    $curl = curl_init();
+      curl_setopt($curl,CURLOPT_URL,$url);
+      curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+      $response = curl_exec($curl);
+      curl_close($curl);
+      $result=json_decode($response,true);
+      pd($result);
+    return $response;
+    
   }
 }
