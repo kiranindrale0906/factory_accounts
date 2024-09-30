dfebc0b2a07483970175f18b6884225ed481f42d
kiran1234 <kiran1234>
2024-09-30 14:20:12 +0530

    chnge

--- application/modules/argold/helpers/chitti_erps_helper.php
+++ application/modules/argold/helpers/chitti_erps_helper.php
@@ -135,7 +135,7 @@ function get_row_actions($row, $url, $select_url, $filter) {
                            'confirm_message' => "",
                            'class' => 'btn-sm');
   $actions["Add Actual Weight"] = array('request' => "http", 
-                           'url' => ADMIN_PATH.'argold/chitti_actual_weights/edit/'.$row['id'],
+                           'url' => ADMIN_PATH.'argold/chitti_actual_weights/edit/'.$row['id'].'factory='.$controller,,
                            'confirm_message' => "",
                            'class' => 'btn-sm');
   $actions["Detail"] = array('request' => "http", 
