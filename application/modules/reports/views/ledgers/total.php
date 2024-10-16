99efba161d241dec1c2deb7d9330b32a6ab4cb33
kiran1234 <kiran1234>
2024-10-16 17:05:19 +0530

    change

--- application/modules/reports/views/ledgers/total.php
+++ application/modules/reports/views/ledgers/total.php
@@ -36,10 +36,10 @@
     <?php endif; ?>
     <?php if ($report_type == 'Vadotar Report' || $report_type == 'Production Report'|| $report_type == 'Summary Report'): 
       if (isset($record['credit_weight']) && $record['credit_weight'] > 0): ?>
-        <td class="text-right"><?= four_decimal($record['factory_fine'] - $record['fine'], '-'); ?></td>
+        <td class="text-right"><?= four_decimal($record['vodatar'] '-'); ?></td>
         <td class="text-right"><?= four_decimal(($record['factory_fine'] - $record['fine']) / $record['credit_weight'] * 100, '-'); ?></td>
       <?php elseif (isset($record['debit_weight']) && $record['debit_weight'] > 0): ?>
-        <td class="text-right"><?= four_decimal($record['vodatar'], '-'); ?></td>
+        <td class="text-right"><?= four_decimal($record['fine'] - $record['factory_fine'], '-'); ?></td>
         <td class="text-right"><?= four_decimal(($record['fine'] - $record['factory_fine']) / $record['debit_weight'] * 100, '-'); ?></td>
       <?php else: ?>
         <td></td>
