d39020968b157b5ecf3a04f7b34ca295445a21f5
kiran1234 <kiran1234>
2025-02-18 17:07:37 +0530

    chnage

--- application/modules/reports/views/production_same_purity_summary/search.php
+++ application/modules/reports/views/production_same_purity_summary/search.php
@@ -5,36 +5,36 @@
   
       <?php if($_SESSION['all_details']==1){?>    
       <a class="ml-5 <?= ($site_name == '') ? 'bold black underline' : '' ?>" 
-         href='<?= base_url() ?>reports/production_same_purity_summary'>All</a>
+         href='<?= base_url() ?>reports/production_same_purity_summary&filter_month=<?=$filter_month ?>&filter_year=<?= $filter_year ?>'>All</a>
       <a class="ml-5 <?= ($site_name == 'ARNA BANGLE ERP' || $site_name == 'ARNA BANGLE') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=ARNA BANGLE ERP'>ARNA BANGLE ERP</a>
+           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=ARNA BANGLE ERP&filter_month=<?=$filter_month ?>&filter_year=<?= $filter_year ?>'>ARNA BANGLE ERP</a>
       <a class="ml-5 <?= ($site_name == 'Rnd Erp Software') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=RND ERP'>RND ERP</a>
+           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=RND ERP&filter_month=<?=$filter_month ?>&filter_year=<?= $filter_year ?>'>RND ERP</a>
        <a class="ml-5 <?= ($site_name == 'Domestic Internal ERP' || $site_name == 'Domestic Internal ERP Software') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=Domestic Internal ERP'>Domestic Internal ERP</a>
+           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=Domestic Internal ERP&filter_month=<?=$filter_month ?>&filter_year=<?= $filter_year ?>'>Domestic Internal ERP</a>
     
       <?php  }?>
       <?php if($_SESSION['arg_details']==1){?>    
         <a class="ml-5 <?= ($site_name == 'AR Gold (Apr 2024)') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=AR Gold (Apr 2024)'>AR Gold (Apr 2024)</a>
+           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=AR Gold (Apr 2024)&filter_month=<?=$filter_month ?>&filter_year=<?= $filter_year ?>'>AR Gold (Apr 2024)</a>
            <a class="ml-5 <?= ($site_name == 'AR Gold ERP' || $site_name == 'ARG ERP Software') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=AR Gold ERP'>AR Gold ERP</a>
+           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=AR Gold ERP&filter_month=<?=$filter_month ?>&filter_year=<?= $filter_year ?>'>AR Gold ERP</a>
      
       <?php }?>
       <?php if($_SESSION['arf_details']==1){?> 
         <a class="ml-5 <?= ($site_name == 'ARF (Apr 2024)') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=ARF (Apr 2024)'>ARF (Apr 2024)</a>   
+           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=ARF (Apr 2024)&filter_month=<?=$filter_month ?>&filter_year=<?= $filter_year ?>'>ARF (Apr 2024)</a>   
         <a class="ml-5 <?= ($site_name == 'ARF (Aug 2024)') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=ARF (Aug 2024)'>ARF (Aug 2024)</a>   
+           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=ARF (Aug 2024)&filter_month=<?=$filter_month ?>&filter_year=<?= $filter_year ?>'>ARF (Aug 2024)</a>   
            <a class="ml-5 <?= ($site_name == 'ARF ERP' || $site_name == 'ARF ERP Software' || $site_name == 'Arf Erp Software') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=ARF ERP'>ARF ERP</a>
+           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=ARF ERP&filter_month=<?=$filter_month ?>&filter_year=<?= $filter_year ?>'>ARF ERP</a>
       
       <?php }?>   
       <?php if($_SESSION['arc_details']==1){?> 
         <a class="ml-5 <?= ($site_name == 'ARC (Apr 2024)') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=ARC (Apr 2024)'>ARC (Apr 2024)</a>   
+           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=ARC (Apr 2024)&filter_month=<?=$filter_month ?>&filter_year=<?= $filter_year ?>'>ARC (Apr 2024)</a>   
            <a class="ml-5 <?= ($site_name == 'ARC ERP' || $site_name == 'ARC ERP Software'|| $site_name == 'Arc Erp Software') ? 'bold black underline' : '' ?>" 
-           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=ARC ERP'>ARC ERP</a>
+           href='<?= base_url() ?>reports/production_same_purity_summary?site_name=ARC ERP&filter_month=<?=$filter_month ?>&filter_year=<?= $filter_year ?>'>ARC ERP</a>
       
       <?php }?>  
        </h6>
@@ -47,17 +47,42 @@
     <h6>
       Group By:
       <a class="ml-5 <?= ($group_by == '') ? 'bold black underline' : '' ?>" 
-         href='<?= base_url() ?>reports/production_same_purity_summary?site_name=<?= $site_name ?>&machine_size=<?= $machine_size ?>&design_code=<?= $design_code ?>&account_name=<?= $account_name ?>&product_name=<?= $product_name ?>&in_purity=<?= $in_purity ?>&category_one=<?= $category_one ?>&group_by='>All</a>
+         href='<?= base_url() ?>reports/production_same_purity_summary?site_name=<?= $site_name ?>&machine_size=<?= $machine_size ?>&design_code=<?= $design_code ?>&account_name=<?= $account_name ?>&product_name=<?= $product_name ?>&in_purity=<?= $in_purity ?>&category_one=<?= $category_one ?>&group_by=&filter_month=<?=$filter_month ?>&filter_year=<?= $filter_year ?>'>All</a>
        <a class="ml-5 <?= ($group_by == 'Date') ? 'bold black underline' : '' ?>" 
-         href='<?= base_url() ?>reports/production_same_purity_summary?site_name=<?= $site_name ?>&machine_size=<?= $machine_size ?>&design_code=<?= $design_code ?>&account_name=<?= $account_name ?>&product_name=<?= $product_name ?>&in_purity=<?= $in_purity ?>&category_one=<?= $category_one ?>&group_by=Date'>Date</a>
+         href='<?= base_url() ?>reports/production_same_purity_summary?site_name=<?= $site_name ?>&machine_size=<?= $machine_size ?>&design_code=<?= $design_code ?>&account_name=<?= $account_name ?>&product_name=<?= $product_name ?>&in_purity=<?= $in_purity ?>&category_one=<?= $category_one ?>&group_by=Date&filter_month=<?=$filter_month ?>&filter_year=<?= $filter_year ?>'>Date</a>
        <a class="ml-5 <?= ($group_by == 'Week') ? 'bold black underline' : '' ?>" 
-         href='<?= base_url() ?>reports/production_same_purity_summary?site_name=<?= $site_name ?>&machine_size=<?= $machine_size ?>&design_code=<?= $design_code ?>&account_name=<?= $account_name ?>&product_name=<?= $product_name ?>&in_purity=<?= $in_purity ?>&category_one=<?= $category_one ?>&group_by=Week'>Week</a>
+         href='<?= base_url() ?>reports/production_same_purity_summary?site_name=<?= $site_name ?>&machine_size=<?= $machine_size ?>&design_code=<?= $design_code ?>&account_name=<?= $account_name ?>&product_name=<?= $product_name ?>&in_purity=<?= $in_purity ?>&category_one=<?= $category_one ?>&group_by=Week&filter_month=<?=$filter_month ?>&filter_year=<?= $filter_year ?>'>Week</a>
        <a class="ml-5 <?= ($group_by == 'Month') ? 'bold black underline' : '' ?>" 
-         href='<?= base_url() ?>reports/production_same_purity_summary?site_name=<?= $site_name ?>&machine_size=<?= $machine_size ?>&design_code=<?= $design_code ?>&account_name=<?= $account_name ?>&product_name=<?= $product_name ?>&in_purity=<?= $in_purity ?>&category_one=<?= $category_one ?>&group_by=Month'>Month</a>
+         href='<?= base_url() ?>reports/production_same_purity_summary?site_name=<?= $site_name ?>&machine_size=<?= $machine_size ?>&design_code=<?= $design_code ?>&account_name=<?= $account_name ?>&product_name=<?= $product_name ?>&in_purity=<?= $in_purity ?>&category_one=<?= $category_one ?>&group_by=Month&filter_month=<?=$filter_month ?>&filter_year=<?= $filter_year ?>'>Month</a>
          <a class="ml-5 <?= ($group_by == 'Year') ? 'bold black underline' : '' ?>" 
-         href='<?= base_url() ?>reports/production_same_purity_summary?site_name=<?= $site_name ?>&machine_size=<?= $machine_size ?>&design_code=<?= $design_code ?>&account_name=<?= $account_name ?>&product_name=<?= $product_name ?>&in_purity=<?= $in_purity ?>&category_one=<?= $category_one ?>&group_by=Year'>Year</a>
+         href='<?= base_url() ?>reports/production_same_purity_summary?site_name=<?= $site_name ?>&machine_size=<?= $machine_size ?>&design_code=<?= $design_code ?>&account_name=<?= $account_name ?>&product_name=<?= $product_name ?>&in_purity=<?= $in_purity ?>&category_one=<?= $category_one ?>&group_by=Year&filter_month=<?=$filter_month ?>&filter_year=<?= $filter_year ?>'>Year</a>
     </h6>
   </div>
 </div>
+<div class="row">
+  <div class="col-md-12">
+
+  <h6>
+       ERP Months (2024):
+  <?php $months = array(1 => 'Jan.', 2 => 'Feb.', 3 => 'Mar.', 4 => 'Apr.', 5 => 'May', 6 => 'Jun.', 7 => 'Jul.', 8 => 'Aug.', 9 => 'Sep.', 10 => 'Oct.', 11 => 'Nov.', 12 => 'Dec.');
+      foreach ($months as $month_key => $month) { ?>
+        <a class="ml-5 <?= ($filter_month == $month_key) ? 'bold black underline' : '' ?>"
+               href='<?= base_url() ?>reports/production_same_purity_summary?site_name=<?= $site_name ?>&machine_size=<?= $machine_size ?>&design_code=<?= $design_code ?>&account_name=<?= $account_name ?>&product_name=<?= $product_name ?>&in_purity=<?= $in_purity ?>&category_one=<?= $category_one ?>&group_by=<?= $group_by ?>&filter_month=<?= $month_key ?>&filter_year=2024'><?= $month ?></a>
+
+          <?php } ?>
+     </h6>
+   </div>
+      <div class="col-md-12">
+  <h6>
+       ERP Months (2025):
+  <?php $new_months = array(1 => 'Jan.', 2 => 'Feb.', 3 => 'Mar.', 4 => 'Apr.', 5 => 'May', 6 => 'Jun.', 7 => 'Jul.', 8 => 'Aug.', 9 => 'Sep.', 10 => 'Oct.', 11 => 'Nov.', 12 => 'Dec.');
+      foreach ($new_months as $new_month_key => $new_month) { ?>
+        <a class="ml-5 <?= ($filter_month == $new_month_key) ? 'bold black underline' : '' ?>"
+               href='<?= base_url() ?>reports/production_same_purity_summary?site_name=<?= $site_name ?>&machine_size=<?= $machine_size ?>&design_code=<?= $design_code ?>&account_name=<?= $account_name ?>&product_name=<?= $product_name ?>&in_purity=<?= $in_purity ?>&category_one=<?= $category_one ?>&group_by=<?= $group_by ?>&filter_month=<?= $new_month_key ?>&filter_year=2025'><?= $new_month ?></a>
+
+          <?php } ?>
+     </h6>
+   </div>
+   </div>
 <br />
       
