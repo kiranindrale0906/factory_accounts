f289d5e0ebb63eb068bb80adcce7004025dc516f
kiran1234 <kiran1234>
2025-06-18 19:09:00 +0530

    change

--- application/modules/masters/models/Item_name_model.php
+++ application/modules/masters/models/Item_name_model.php
@@ -15,18 +15,14 @@ class Item_name_model extends BaseModel {
         'field' => 'item_names[name]', 
         'label' => 'name', 
         'rules'  =>array('trim','required',
-                    array('check_name_error',array($this,'check_name_exist'))),
-        'errors' => array('check_name_error'=>'Name is alreay exist.')),
+                    array('check_name_error',array($this,'check_duplicate_name'))),
+        'errors' => array('check_name_error'=>'Item name already exists.')),
       
       );
   }
 
-  public function check_name_exist($name) {
-    if($name=="" && !isset($name))
-      return true;
-    else
-    $item_names=$this->item_name_model->find('id as id',array('name!='=>$name));
-    return (empty($item_names)) ? false : true;
+  public function check_duplicate_name($name) {
+    return parent::check_unique('name');
   }
   
 }
